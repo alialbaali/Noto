bb1af8d5b22d7d10856ef8805b4ce263b3d288b4
Ali Albaali <alialbaali@protonmail.ch>
2020-07-04 20:54:58 +0300

    Add Bottom App Bar and overhaul the Library Fragment Design

--- app/src/main/java/com/noto/library/LibraryFragment.kt
+++ app/src/main/java/com/noto/library/LibraryFragment.kt
@@ -1,153 +1,138 @@
 package com.noto.library
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.google.android.material.appbar.AppBarLayout
 import com.noto.NotoItemTouchHelper
 import com.noto.R
 import com.noto.databinding.FragmentLibraryBinding
+import com.noto.domain.model.Noto
 import com.noto.domain.model.NotoColor
+import com.noto.domain.model.NotoIcon
 import com.noto.util.getValue
 import com.noto.util.setFontFamily
-import org.joda.time.format.DateTimeFormat
 import org.koin.android.viewmodel.ext.android.viewModel
-import kotlin.math.abs
 
-class LibraryFragment : Fragment(), NotoClickListener {
+class LibraryFragment : Fragment() {
 
-    private val binding by lazy {
-        FragmentLibraryBinding.inflate(layoutInflater,null, false).also {
-            it.lifecycleOwner = this
-        }
-    }
-
-    private val rvAdapter by lazy { NotoListRVAdapter(viewModel, args.notoColor, this) }
-
-    private val rvLayoutManager by lazy { StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL) }
-
-    private val args by navArgs<LibraryFragmentArgs>()
+    private lateinit var binding: FragmentLibraryBinding
 
     private val viewModel by viewModel<LibraryViewModel>()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    private val args by navArgs<LibraryFragmentArgs>()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        binding = FragmentLibraryBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = this@LibraryFragment
+        }
+
+        binding.ctb.setFontFamily()
+
+        binding.tb.setNavigationOnClickListener { findNavController().navigateUp() }
 
         viewModel.getLibraryById(args.libraryId)
-
         viewModel.getNotos(args.libraryId)
 
-        setListeners()
+        with(binding.fab) {
 
-        binding.fab.imageTintList = ColorStateList.valueOf(resources.getColor(R.color.colorPrimary))
+            imageTintList = ResourcesCompat.getColorStateList(resources, R.color.colorBackground, null)
 
-        val formatter = DateTimeFormat.mediumDate()
-
-        viewModel.library.observe(viewLifecycleOwner, Observer {
-            it?.let {
-                binding.ctb.title = it.libraryTitle
-                binding.tvLibraryTitle.text = it.libraryTitle
-                binding.tvLibraryCreationDate.text = formatter.print(it.libraryCreationDate)
-
-                val notosCount = viewModel.countNotos(args.libraryId)
-                if (notosCount == 1) {
-                    binding.tvLibraryNotoCount.text = notosCount.toString().plus(" Noto")
-                } else {
-                    binding.tvLibraryNotoCount.text = notosCount.toString().plus(" Notos")
-                }
-
-                setNoto(args.notoColor)
+            setOnClickListener {
+                findNavController().navigate(LibraryFragmentDirections.actionLibraryFragmentToNotoFragment(args.libraryId))
             }
-        })
 
-        binding.tb.setOnMenuItemClickListener {
-            when (it.itemId){
-                R.id.delete_library -> {
-                    viewModel.deleteLibrary()
-                    this.findNavController().navigateUp()
+        }
+
+        with(binding.bab) {
+
+            navigationIcon?.mutate()?.setTint(ResourcesCompat.getColor(resources, R.color.colorPrimary, null))
+
+            setNavigationOnClickListener {
+
+            }
+
+
+            setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.archived_notos -> {
+                        findNavController().navigate(LibraryFragmentDirections.actionLibraryFragmentToArchiveFragment(args.libraryId))
+                        true
+                    }
+
+                    R.id.delete_library -> {
+                        findNavController().navigateUp()
+                        viewModel.deleteLibrary()
+                        true
+                    }
+
+                    else -> false
                 }
             }
-            true
+
+        }
+
+        viewModel.library.observe(viewLifecycleOwner) { library ->
+
+            setLibraryColors(library.notoColor, library.notoIcon)
+            binding.ctb.title = library.libraryTitle
+            binding.tvLibraryTitle.text = library.libraryTitle
+
+            val notosCount = viewModel.countNotos(library.libraryId)
+            binding.tvLibraryNotoCount.text = notosCount.toString().plus(if (notosCount == 1) " Noto" else " Notos")
+
         }
 
         with(binding.rv) {
 
+            val rvAdapter = NotoListRVAdapter(viewModel,
+                object : NotoItemClickListener {
+                    override fun onClick(noto: Noto) {
+                        findNavController().navigate(LibraryFragmentDirections.actionLibraryFragmentToNotoFragment(noto.libraryId, noto.notoId))
+                    }
+
+                })
+
             adapter = rvAdapter
-            layoutManager = rvLayoutManager
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
 
             NotoItemTouchHelper(rvAdapter).let {
                 ItemTouchHelper(it).attachToRecyclerView(this)
             }
 
             viewModel.notos.observe(viewLifecycleOwner, Observer {
-                this@LibraryFragment.rvAdapter.submitList(it)
-//                setList(it, rvAdapter, binding.emptyNotebook)
+                rvAdapter.submitList(it)
             })
         }
-
-        binding.abl.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
-
-            if (abs(verticalOffset) == appBarLayout.totalScrollRange) {
-                binding.tb.title = binding.tvLibraryTitle.text
-                // Collapsed
-            } else if (verticalOffset == 0) {
-                binding.tb.title = ""
-                // Expanded
-            } else {
-                binding.tb.title = ""
-                // Somewhere in between
-            }
-        })
 
         return binding.root
     }
 
-    private fun setNoto(notoColor: NotoColor) {
-        with(binding) {
-            val color = resources.getColor(notoColor.getValue())
-            requireActivity().window?.statusBarColor = color
-            tb.setBackgroundColor(color)
-            ctb.setContentScrimColor(color)
-            ctb.setFontFamily()
-            ctb.backgroundTintList = ColorStateList.valueOf(color)
-            fab.backgroundTintList = ColorStateList.valueOf(color)
-        }
+    private fun setLibraryColors(notoColor: NotoColor, notoIcon: NotoIcon) {
+
+        val color = ResourcesCompat.getColor(resources, notoColor.getValue(), null)
+
+        binding.tvLibraryTitle.setTextColor(color)
+        binding.tvLibraryNotoCount.setTextColor(color)
+        binding.tb.navigationIcon?.mutate()?.setTint(color)
+        binding.ivLibraryIcon.setImageResource(notoIcon.getValue())
+        binding.ivLibraryIcon.imageTintList = ResourcesCompat.getColorStateList(resources, notoColor.getValue(), null)
+        binding.fab.backgroundTintList = ResourcesCompat.getColorStateList(resources, notoColor.getValue(), null)
+
     }
 
-    private fun setListeners() {
-        binding.fab.setOnClickListener {
-            this.findNavController().navigate(
-//                LibraryFragmentDirections.actionLibraryFragmentToNotoNewFragment()
-                LibraryFragmentDirections.actionLibraryFragmentToNotoFragment(
-                    args.libraryId,
-                    0
-                )
-            )
-//            this.findNavController().navigate(LibraryFragmentDirections.actionLibraryFragmentToNotoFragment(args.libraryId, 0))
-        }
+    private fun getAllNotos() {
 
-        binding.tb.setNavigationOnClickListener {
-            this.findNavController().navigateUp()
-        }
-    }
+        viewModel.getAllNotos()
 
-    override fun onClick(id: Long) {
-        this.findNavController().navigate(
-//            LibraryFragmentDirections.actionLibraryFragmentToNotoNewFragment()
-            LibraryFragmentDirections.actionLibraryFragmentToNotoFragment(
-                args.libraryId,
-                id
-            )
-        )
     }
 }
