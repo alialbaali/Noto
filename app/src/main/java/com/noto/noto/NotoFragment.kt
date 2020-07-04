9f53464c2b4d3d04d2368d21f38ab6b17bef0dc5
Ali Albaali <alialbaali@protonmail.ch>
2020-07-04 20:55:32 +0300

    Add Bottom App Bar to the Noto Fragment

--- app/src/main/java/com/noto/noto/NotoFragment.kt
+++ app/src/main/java/com/noto/noto/NotoFragment.kt
@@ -7,133 +7,147 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.addCallback
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.noto.R
 import com.noto.databinding.FragmentNotoBinding
-import com.noto.library.LibraryListViewModel
 import com.noto.util.getValue
-import org.koin.android.viewmodel.ext.android.viewModel
+import com.noto.util.snackbar
+import org.koin.android.viewmodel.ext.android.sharedViewModel
+
+const val NOTO_ID = "noto_id"
+const val NOTO_TITLE = "noto_title"
+const val NOTO_BODY = "noto_body!"
 
 class NotoFragment : Fragment() {
 
-    // Binding
-    private val binding by lazy {
-        FragmentNotoBinding.inflate(layoutInflater).also {
-            it.lifecycleOwner = this
-            it.viewModel = viewModel
-        }
-    }
+    private lateinit var binding: FragmentNotoBinding
 
-    private val viewModel by viewModel<NotoViewModel>()
+    private val viewModel by sharedViewModel<NotoViewModel>()
 
     private val args by navArgs<NotoFragmentArgs>()
 
-    private val imm by lazy { requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager }
-
-    private val libraryListViewModel by viewModel<LibraryListViewModel>()
+    private val imm by lazy { requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        imm.showSoftInput(binding.etNotoBody, InputMethodManager.SHOW_IMPLICIT)
 
-        viewModel.getNoto(args.libraryId, args.notoId)
+        binding = FragmentNotoBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = this@NotoFragment
+            viewModel = this@NotoFragment.viewModel
+        }
+
         viewModel.getLibraryById(args.libraryId)
-        viewModel.getLabels()
 
-//        viewModel.getNotoById(args.libraryId, args.notoId)
+        if (args.notoId == 0L) {
 
-        binding.tb.let { tb ->
+            viewModel.postNoto(args.libraryId)
 
-            tb.setNavigationOnClickListener {
+            requireActivity().onBackPressedDispatcher.addCallback(this) {
+                this@NotoFragment.findNavController().navigateUp()
+                viewModel.createNoto()
+            }.isEnabled = true
+
+            binding.tb.setNavigationOnClickListener {
                 imm.hideSoftInputFromWindow(binding.etNotoBody.windowToken, InputMethodManager.HIDE_IMPLICIT_ONLY)
-                this.findNavController().navigateUp()
-                viewModel.saveNoto(args.notoId)
+                findNavController().navigateUp()
+                viewModel.createNoto()
             }
+
+        } else {
+
+            viewModel.getNotoById(args.notoId)
+
+            requireActivity().onBackPressedDispatcher.addCallback(this) {
+                this@NotoFragment.findNavController().navigateUp()
+                viewModel.updateNoto()
+            }.isEnabled = true
+
+
+            binding.tb.setNavigationOnClickListener {
+                imm.hideSoftInputFromWindow(binding.etNotoBody.windowToken, InputMethodManager.HIDE_IMPLICIT_ONLY)
+                findNavController().navigateUp()
+                viewModel.updateNoto()
+            }
+
         }
 
-        binding.tb.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.delete_noto -> {
-                    viewModel.deleteNoto()
-                    this.findNavController().navigateUp()
-                    binding.etNotoBody.clearFocus()
+
+        with(binding.fab) {
+
+            imageTintList = ResourcesCompat.getColorStateList(resources, R.color.colorBackground, null)
+
+            setOnClickListener {
+                findNavController().navigate(NotoFragmentDirections.actionNotoFragmentToReminderDialogFragment())
+            }
+
+        }
+
+        with(binding.bab) {
+
+            navigationIcon?.mutate()?.setTint(ResourcesCompat.getColor(resources, R.color.colorPrimary, null))
+
+            setNavigationOnClickListener {
+
+            }
+
+            setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.delete_noto -> {
+                        findNavController().navigateUp()
+                        viewModel.deleteNoto()
+                        true
+                    }
+
+                    R.id.archive_noto -> {
+                        menuItem.actionView?.animate()?.translationY(100F)?.start()
+                        menuItem.icon = ResourcesCompat.getDrawable(resources, R.drawable.ic_outline_unarchive_24, null)
+
+                        if (viewModel.noto.value?.notoIsArchived == true) {
+                            viewModel.setArchived(false)
+                            binding.root.snackbar(getString(R.string.noto_unarchived))
+                        } else {
+                            viewModel.setArchived(true)
+                            binding.root.snackbar(getString(R.string.noto_archived))
+                        }
+
+                        true
+                    }
+
+                    else -> false
                 }
-//                R.id.library -> {
-//                    LibrarySelectorDialog(requireContext(), libraryListViewModel, viewModel, args.notoId)
-//                }
-//                R.id.schedule -> ScheduleDialog(requireContext())
             }
-            true
         }
 
-        requireActivity().onBackPressedDispatcher.addCallback(this) {
-            this@NotoFragment.findNavController().navigateUp()
-//            viewModel.saveNoto(args.libraryId, args.notoId)
-            viewModel.saveNoto(args.notoId)
-        }.isEnabled = true
+        val archiveMenuItem = binding.bab.menu.findItem(R.id.archive_noto)
 
+        viewModel.noto.observe(viewLifecycleOwner) { noto ->
 
-        viewModel.library.observe(viewLifecycleOwner, Observer {
-            it?.let {
-                binding.tb.title = it.libraryTitle
-                binding.tb.setBackgroundResource(it.notoColor.getValue())
-            }
-        })
-//        binding.etNotoBody.requestFocus()
-//        imm.showSoftInput(binding.etNotoBody, InputMethodManager.SHOW_FORCED)
-//
-//        binding.tv.setOnClickListener {
-//            LibrarySelectorDialog(requireContext(), libraryListViewModel, viewModel, args.notoId)
-//        }
+            if (noto.notoIsArchived) archiveMenuItem.icon = ResourcesCompat.getDrawable(resources, R.drawable.ic_outline_unarchive_24, null)
+            else archiveMenuItem.icon = ResourcesCompat.getDrawable(resources, R.drawable.archive_arrow_down_outline, null)
 
-//        viewModel.notoLabels.observe(viewLifecycleOwner, Observer { labels ->
-//
-//            Timber.i("CHANGED")
-//
-//            for (label in labels) {
-//
-//                Chip(requireContext()).also { chip ->
-//
-//                    chip.id = label.labelId.toInt()
-//
-//                    chip.chipBackgroundColor = resources.getColorStateList(label.notoColor.getValue())
-//
-//                    chip.text = label.labelTitle
-//
-//                    chip.gravity = Gravity.CENTER
-//
-//                    chip.setTextColor(resources.getColor(R.color.colorPrimary))
-//
-//                    chip.closeIconTint = resources.getColorStateList(R.color.colorOnPrimary)
-//
-//                    chip.isCheckable = false
-//
-//                    chip.isCloseIconVisible = false
-//
-//                    chip.setOnClickListener {
-//                        chip.isCloseIconVisible = !chip.isCloseIconVisible
-//                    }
-//
-//                    chip.setOnCloseIconClickListener {
-//                        viewModel.notoLabels.value!!.remove(label)
-//                    }
-//
-//                    binding.cgLabels.addView(chip)
-//                }
-//            }
-//
-//        })
+            if (noto.notoReminder == null) binding.fab.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.bell_plus_outline, null))
+            else binding.fab.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.bell_ring_outline, null))
 
-//        binding.cpAdd.setOnClickListener {
-//            LabelSelectorDialog(requireContext(), viewModel)
-//        }
+        }
+
+        viewModel.library.observe(viewLifecycleOwner) { library ->
+            val color = ResourcesCompat.getColor(resources, library.notoColor.getValue(), null)
+
+            binding.tb.title = library.libraryTitle
+            binding.tb.setTitleTextColor(color)
+            binding.tb.navigationIcon?.mutate()?.setTint(color)
+            binding.fab.backgroundTintList = ResourcesCompat.getColorStateList(resources, library.notoColor.getValue(), null)
+        }
 
         return binding.root
     }
+
+
 }
