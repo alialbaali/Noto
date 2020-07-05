6af373450c15b47e06b85a67a21c12ad5ba821f8
Ali Albaali <alialbaali@protonmail.ch>
2020-07-06 01:35:22 +0300

    Add RV Layout Manager Options and their animations

--- app/src/main/java/com/noto/library/LibraryFragment.kt
+++ app/src/main/java/com/noto/library/LibraryFragment.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.android.material.appbar.AppBarLayout
 import com.noto.NotoItemTouchHelper
 import com.noto.R
 import com.noto.databinding.FragmentLibraryBinding
@@ -75,6 +78,17 @@ class LibraryFragment : Fragment() {
                         true
                     }
 
+                    R.id.view -> {
+
+                        when (viewModel.layoutManager.value) {
+                            LINEAR_LAYOUT_MANAGER -> viewModel.setLayoutManager(GRID_LAYOUT_MANAGER)
+                            GRID_LAYOUT_MANAGER -> viewModel.setLayoutManager(STAGGERED_LAYOUT_MANAGER)
+                            STAGGERED_LAYOUT_MANAGER -> viewModel.setLayoutManager(LINEAR_LAYOUT_MANAGER)
+                        }
+
+                        true
+                    }
+
                     else -> false
                 }
             }
@@ -84,8 +98,8 @@ class LibraryFragment : Fragment() {
         viewModel.library.observe(viewLifecycleOwner) { library ->
 
             setLibraryColors(library.notoColor, library.notoIcon)
-            binding.ctb.title = library.libraryTitle
             binding.tvLibraryTitle.text = library.libraryTitle
+            binding.tvPlaceHolder.text = library.libraryTitle
 
             val notosCount = viewModel.countNotos(library.libraryId)
             binding.tvLibraryNotoCount.text = notosCount.toString().plus(if (notosCount == 1) " Noto" else " Notos")
@@ -96,22 +110,60 @@ class LibraryFragment : Fragment() {
 
             val rvAdapter = NotoListRVAdapter(viewModel,
                 object : NotoItemClickListener {
+
                     override fun onClick(noto: Noto) {
                         findNavController().navigate(LibraryFragmentDirections.actionLibraryFragmentToNotoFragment(noto.libraryId, noto.notoId))
+                    }
+
+                    override fun onLongClick(noto: Noto) {
+                        findNavController().navigate(LibraryFragmentDirections.actionLibraryFragmentToNotoDialogFragment(noto.libraryId, noto.notoId))
                     }
 
                 })
 
             adapter = rvAdapter
-            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
 
             NotoItemTouchHelper(rvAdapter).let {
                 ItemTouchHelper(it).attachToRecyclerView(this)
             }
 
-            viewModel.notos.observe(viewLifecycleOwner, Observer {
-                rvAdapter.submitList(it)
-            })
+            val layoutManagerMenuItem = binding.bab.menu.findItem(R.id.view)
+
+            viewModel.layoutManager.observe(viewLifecycleOwner) { value ->
+
+                when (value) {
+                    LINEAR_LAYOUT_MANAGER -> {
+                        layoutManagerMenuItem.icon = ResourcesCompat.getDrawable(resources, R.drawable.view_grid_outline, null)
+                        layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+                    }
+                    GRID_LAYOUT_MANAGER -> {
+                        layoutManagerMenuItem.icon = ResourcesCompat.getDrawable(resources, R.drawable.view_dashboard_outline, null)
+                        layoutManager = GridLayoutManager(requireContext(), 2)
+                    }
+                    STAGGERED_LAYOUT_MANAGER -> {
+                        layoutManagerMenuItem.icon = ResourcesCompat.getDrawable(resources, R.drawable.view_agenda_outline, null)
+                        layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+                    }
+                }
+
+                visibility = View.INVISIBLE
+                startAnimation(AnimationUtils.loadAnimation(requireContext(), R.anim.hide))
+
+                visibility = View.VISIBLE
+                startAnimation(AnimationUtils.loadAnimation(requireContext(), R.anim.show))
+
+            }
+
+            viewModel.notos.observe(viewLifecycleOwner) {
+                if (it.isEmpty()) {
+                    listOf(binding.tvLibraryNotoCount, binding.tvLibraryTitle, binding.ivLibraryIcon).forEach { it.visibility = View.GONE }
+                    binding.llPlaceHolder.visibility = View.VISIBLE
+                    val layoutParams = binding.ctb.layoutParams as AppBarLayout.LayoutParams
+                    layoutParams.scrollFlags = 0
+                } else {
+                    rvAdapter.submitList(it)
+                }
+            }
         }
 
         return binding.root
@@ -128,11 +180,9 @@ class LibraryFragment : Fragment() {
         binding.ivLibraryIcon.imageTintList = ResourcesCompat.getColorStateList(resources, notoColor.getValue(), null)
         binding.fab.backgroundTintList = ResourcesCompat.getColorStateList(resources, notoColor.getValue(), null)
 
+        binding.tvPlaceHolder.setTextColor(color)
+        binding.ivPlaceHolder.setImageResource(notoIcon.getValue())
+        binding.ivPlaceHolder.imageTintList = ResourcesCompat.getColorStateList(resources, notoColor.getValue(), null)
     }
 
-    private fun getAllNotos() {
-
-        viewModel.getAllNotos()
-
-    }
 }
--- app/src/main/java/com/noto/library/LibraryViewModel.kt
+++ app/src/main/java/com/noto/library/LibraryViewModel.kt
@@ -1,24 +1,30 @@
 package com.noto.library
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.core.content.edit
+import androidx.lifecycle.*
 import com.noto.domain.interactor.library.LibraryUseCases
 import com.noto.domain.interactor.noto.NotoUseCases
 import com.noto.domain.model.Library
 import com.noto.domain.model.Noto
+import com.tfcporciuncula.flow.FlowSharedPreferences
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-class LibraryViewModel(private val libraryUseCases: LibraryUseCases, private val notoUseCases: NotoUseCases) : ViewModel() {
+private const val LAYOUT_MANAGER_KEY = "Library_Layout_Manager"
+
+class LibraryViewModel(private val libraryUseCases: LibraryUseCases, private val notoUseCases: NotoUseCases, private val storage: FlowSharedPreferences) : ViewModel() {
 
     private val _library = MutableLiveData<Library>()
     val library: LiveData<Library> = _library
 
     private var _notos = MutableLiveData<List<Noto>>()
     val notos: LiveData<List<Noto>> = _notos
+
+    val layoutManager = liveData<Int> {
+        val flow = storage.getInt(LAYOUT_MANAGER_KEY, LINEAR_LAYOUT_MANAGER).asFlow()
+        emitSource(flow.asLiveData())
+    }
 
     fun getNotos(libraryId: Long) {
         viewModelScope.launch {
@@ -27,7 +33,24 @@ class LibraryViewModel(private val libraryUseCases: LibraryUseCases, private val
                     _notos.postValue(list)
                 }
             }
-//            _notos.postValue(sort(notoRepository.getNotos(libraryId)))
+        }
+    }
+
+    fun getArchivedNotos(libraryId: Long) = viewModelScope.launch {
+
+        notoUseCases.getArchivedNotos().onSuccess { flow ->
+            flow.collect {
+                if (libraryId == 0L) _notos.postValue(it) else _notos.postValue(it.filter { it.libraryId == libraryId })
+            }
+        }
+
+    }
+
+    fun getAllNotos() = viewModelScope.launch {
+        notoUseCases.getAllNotos().onSuccess { flow ->
+            flow.collect {
+                _notos.postValue(it)
+            }
         }
     }
 
@@ -53,6 +76,9 @@ class LibraryViewModel(private val libraryUseCases: LibraryUseCases, private val
             libraryUseCases.deleteLibrary(library.value!!)
         }
     }
+
+
+    fun setLayoutManager(value: Int) = storage.sharedPreferences.edit { putInt(LAYOUT_MANAGER_KEY, value) }
 //
 //    private fun sort(notos: List<Noto>): List<Noto> {
 //        return if (library.value?.sortType == SortType.ASC) {
