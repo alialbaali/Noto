88ab69934c006c74dea32db60596e11df3d6c0c1
Ali Albaali <alialbaali@pm.me>
2020-07-21 14:17:23 +0300

    Add Noto Dialog Fragment

---
+++ app/src/main/java/com/noto/noto/NotoDialogFragment.kt
@@ -0,0 +1,120 @@
+package com.noto.noto
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.lifecycle.observe
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.noto.BaseBottomSheetDialogFragment
+import com.noto.ConfirmationDialogFragment
+import com.noto.R
+import com.noto.databinding.FragmentDialogNotoBinding
+import com.noto.util.getValue
+import com.noto.util.toast
+import org.koin.android.viewmodel.ext.android.sharedViewModel
+
+class NotoDialogFragment : BaseBottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentDialogNotoBinding
+
+    private val viewModel by sharedViewModel<NotoViewModel>()
+
+    private val args by navArgs<NotoDialogFragmentArgs>()
+
+    private val clipboardManager by lazy { requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        binding = FragmentDialogNotoBinding.inflate(inflater, container, false)
+
+        if (args.notoId != 0L) viewModel.getNotoById(args.notoId)
+
+        viewModel.getLibraryById(args.libraryId)
+
+        viewModel.library.observe(viewLifecycleOwner) {
+            binding.vHead.backgroundTintList = ResourcesCompat.getColorStateList(resources, it.notoColor.getValue(), null)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                listOf(binding.tvCopyToClipboard, binding.tvShareNoto, binding.tvArchiveNoto, binding.tvRemindMe).forEach { tv ->
+                    tv.compoundDrawableTintList = ResourcesCompat.getColorStateList(resources, it.notoColor.getValue(), null)
+                }
+            }
+        }
+
+        viewModel.noto.observe(viewLifecycleOwner) { noto ->
+
+            binding.tvArchiveNoto.compoundDrawablesRelative[0] =
+                if (noto.notoIsArchived) ResourcesCompat.getDrawable(resources, R.drawable.ic_outline_unarchive_24, null)
+                else ResourcesCompat.getDrawable(resources, R.drawable.archive_arrow_down_outline, null)
+
+            binding.tvRemindMe.compoundDrawablesRelative[0] =
+                if (noto.notoReminder == null) ResourcesCompat.getDrawable(resources, R.drawable.bell_plus_outline, null)
+                else ResourcesCompat.getDrawable(resources, R.drawable.bell_ring_outline, null)
+
+        }
+
+        binding.tvArchiveNoto.setOnClickListener {
+            dismiss()
+            if (viewModel.noto.value?.notoIsArchived == true) viewModel.setArchived(false) else viewModel.setArchived(true)
+            viewModel.updateNoto()
+        }
+
+        binding.tvRemindMe.setOnClickListener {
+            dismiss()
+            findNavController().navigate(NotoDialogFragmentDirections.actionNotoDialogFragmentToReminderDialogFragment(args.notoId))
+        }
+
+        binding.tvCopyToClipboard.setOnClickListener { v ->
+            dismiss()
+            val clipData = ClipData.newPlainText(viewModel.library.value?.libraryTitle, "${viewModel.noto.value?.notoTitle}\n${viewModel.noto.value?.notoBody}")
+            clipboardManager.setPrimaryClip(clipData)
+            v.toast(getString(R.string.copied_to_clipboard))
+        }
+
+        binding.tvShareNoto.setOnClickListener {
+
+            dismiss()
+
+            val noto = viewModel.noto.value
+
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_MIME_TYPES, noto?.notoTitle)
+                putExtra(Intent.EXTRA_MIME_TYPES, noto?.notoBody)
+            }
+
+            val chooser = Intent.createChooser(intent, "${getString(R.string.share)} ${noto?.notoTitle} to")
+
+            startActivity(chooser)
+
+        }
+
+        binding.tvDeleteNoto.setOnClickListener {
+
+            val navController = findNavController()
+
+            ConfirmationDialogFragment { dialogFragment, dialogBinding ->
+
+                dialogBinding.btnConfirm.text = dialogFragment.getString(R.string.delete_noto)
+                dialogBinding.tvTitle.text = dialogFragment.getString(R.string.delete_noto_confirmation)
+
+                dialogBinding.btnConfirm.setOnClickListener {
+                    dialogFragment.dismiss()
+                    navController.navigate(NotoDialogFragmentDirections.actionNotoDialogFragmentToLibraryFragment(args.libraryId))
+                    viewModel.deleteNoto()
+                }
+            }.show(parentFragmentManager, null)
+        }
+
+
+        return binding.root
+    }
+
+}
