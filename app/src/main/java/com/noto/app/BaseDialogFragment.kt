5d3627de9ba771da555927d6421315769cf12faa
Ali Albaali <ali@albaali.com>
2021-08-05 17:46:46 +0300

    Set dialog fully expanded by default

--- app/src/main/java/com/noto/app/BaseDialogFragment.kt
+++ app/src/main/java/com/noto/app/BaseDialogFragment.kt
@@ -1,9 +1,18 @@
 package com.noto.app
 
+import android.app.Dialog
+import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 open class BaseDialogFragment : BottomSheetDialogFragment() {
 
     override fun getTheme(): Int = R.style.BottomSheetDialog
 
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState).apply {
+            (this as BottomSheetDialog).behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+    }
 }
