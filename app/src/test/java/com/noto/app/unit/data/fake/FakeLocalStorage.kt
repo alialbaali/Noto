c2d241b6494dd9c574fea4c5b6f15e2ed4e8c12e
Ali Albaali <ali@albaali.com>
2021-08-11 00:14:53 +0300

    Add FakeLocalStorage test double

---
+++ app/src/test/java/com/noto/app/unit/data/fake/FakeLocalStorage.kt
@@ -0,0 +1,24 @@
+package com.noto.app.unit.data.fake
+
+import com.noto.app.domain.source.LocalStorage
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.mapNotNull
+
+class FakeLocalStorage : LocalStorage {
+    private val storage = MutableStateFlow<MutableMap<String, String>>(mutableMapOf())
+
+    override fun get(key: String): Flow<String> = storage.mapNotNull { it[key] }
+
+    override suspend fun put(key: String, value: String) {
+        storage.value[key] = value
+    }
+
+    override suspend fun remove(key: String) {
+        storage.value.remove(key)
+    }
+
+    override suspend fun clear() {
+        storage.value.clear()
+    }
+}
