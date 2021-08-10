5261c4f888ef8e8c3bb602350873d0d92bd7b64f
Ali Albaali <ali@albaali.com>
2021-08-11 00:26:12 +0300

    Use StateFlow in fake data sources

--- app/src/test/java/com/noto/app/unit/data/fake/FakeLocalLibraryDataSource.kt
+++ app/src/test/java/com/noto/app/unit/data/fake/FakeLocalLibraryDataSource.kt
@@ -3,26 +3,27 @@ package com.noto.app.unit.data.fake
 import com.noto.app.domain.model.Library
 import com.noto.app.domain.source.LocalLibraryDataSource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
 
 class FakeLocalLibraryDataSource : LocalLibraryDataSource {
+    private val libraries = MutableStateFlow<MutableList<Library>>(mutableListOf())
 
-    private val libraries = mutableListOf<Library>()
+    override fun getLibraries(): Flow<List<Library>> = libraries
 
-    override fun getLibraries(): Flow<List<Library>> = flowOf(libraries)
-
-    override fun getLibrary(libraryId: Long): Flow<Library> = flowOf(libraries.first { it.id == libraryId })
+    override fun getLibrary(libraryId: Long): Flow<Library> = libraries
+        .map { it.first { it.id == libraryId } }
 
     override suspend fun createLibrary(library: Library) {
-        libraries.add(library)
+        libraries.value.add(library)
     }
 
     override suspend fun updateLibrary(library: Library) {
-        val libraryIndex = libraries.indexOfFirst { it.id == library.id }
-        libraries[libraryIndex] = library
+        val libraryIndex = libraries.value.indexOfFirst { it.id == library.id }
+        libraries.value[libraryIndex] = library
     }
 
     override suspend fun deleteLibrary(library: Library) {
-        libraries.remove(library)
+        libraries.value.remove(library)
     }
 }
--- app/src/test/java/com/noto/app/unit/data/fake/FakeLocalNoteDataSource.kt
+++ app/src/test/java/com/noto/app/unit/data/fake/FakeLocalNoteDataSource.kt
@@ -3,30 +3,36 @@ package com.noto.app.unit.data.fake
 import com.noto.app.domain.model.Note
 import com.noto.app.domain.source.LocalNoteDataSource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 
 class FakeLocalNoteDataSource : LocalNoteDataSource {
+    private val notes = MutableStateFlow<MutableList<Note>>(mutableListOf())
 
-    private val notes = mutableListOf<Note>()
+    override fun getNotesByLibraryId(libraryId: Long): Flow<List<Note>> = notes
+        .map { it.filter { it.libraryId == libraryId && !it.isArchived } }
 
-    override fun getNotesByLibraryId(libraryId: Long): Flow<List<Note>> = flowOf(notes.filter { it.libraryId == libraryId && !it.isArchived })
+    override fun getArchivedNotesByLibraryId(libraryId: Long): Flow<List<Note>> = notes
+        .map { it.filter { it.libraryId == libraryId && it.isArchived } }
 
-    override fun getArchivedNotesByLibraryId(libraryId: Long): Flow<List<Note>> = flowOf(notes.filter { it.libraryId == libraryId && it.isArchived })
-
-    override fun getNoteById(noteId: Long): Flow<Note> = flowOf(notes.first { it.id == noteId })
+    override fun getNoteById(noteId: Long): Flow<Note> = notes
+        .map { it.first { it.id == noteId } }
 
     override suspend fun createNote(note: Note) {
-        notes.add(note)
+        notes.value.add(note)
     }
 
     override suspend fun updateNote(note: Note) {
-        val noteIndex = notes.indexOfFirst { it.id == note.id }
-        notes[noteIndex] = note
+        val noteIndex = notes.value.indexOfFirst { it.id == note.id }
+        notes.value[noteIndex] = note
     }
 
     override suspend fun deleteNote(note: Note) {
-        notes.remove(note)
+        notes.value.remove(note)
     }
 
-    override suspend fun countLibraryNotes(libraryId: Long): Int = notes.count { it.libraryId == libraryId && !it.isArchived }
+    override suspend fun countLibraryNotes(libraryId: Long): Int = notes
+        .map { it.count { it.libraryId == libraryId && !it.isArchived } }
+        .first()
 }
