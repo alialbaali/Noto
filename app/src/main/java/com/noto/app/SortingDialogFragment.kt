36bde61de81bc19dce031e396807818e1eb0b716
Ali Albaali <ali@albaali.com>
2021-08-16 03:20:30 +0300

    Redesign SortingDialogFragment

--- app/src/main/java/com/noto/app/SortingDialogFragment.kt
+++ app/src/main/java/com/noto/app/SortingDialogFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AlphaAnimation
-import androidx.core.widget.TextViewCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import com.noto.app.databinding.BaseDialogFragmentBinding
@@ -43,60 +41,29 @@ class SortingDialogFragment : BaseDialogFragment() {
                 val resourceColorState = resources.colorStateResource(it.color.toResource())
 
                 baseDialog.tvDialogTitle.setTextColor(resourceColor)
-                baseDialog.vHead.backgroundTintList = resources.colorStateResource(it.color.toResource())
+                baseDialog.vHead.backgroundTintList = resourceColorState
 
-                listOf(tvAlphabetically, tvCreationDate)
-                    .forEach { TextViewCompat.setCompoundDrawableTintList(it, resourceColorState) }
-
-                val selectedElevation = resources.dimenResource(R.dimen.elevation_normal)
+                rbSortingAsc.buttonTintList = resourceColorState
+                rbSortingDesc.buttonTintList = resourceColorState
+                rbAlphabetically.buttonTintList = resourceColorState
+                rbCreationDate.buttonTintList = resourceColorState
 
                 when (it.sortingMethod) {
-                    SortingMethod.Asc -> {
-                        rbSortingAsc.isChecked = true
-                        rbSortingAsc.elevation = selectedElevation
-                        rbSortingAsc.backgroundTintList = resourceColorState
-                        rbSortingDesc.backgroundTintList = resources.colorStateResource(R.color.colorSurface)
-                        rbSortingDesc.elevation = 0F
-                    }
-                    SortingMethod.Desc -> {
-                        rbSortingDesc.isChecked = true
-                        rbSortingDesc.elevation = selectedElevation
-                        rbSortingDesc.backgroundTintList = resourceColorState
-                        rbSortingAsc.backgroundTintList = resources.colorStateResource(R.color.colorSurface)
-                        rbSortingAsc.elevation = 0F
-                    }
+                    SortingMethod.Asc -> rbSortingAsc.isChecked = true
+                    SortingMethod.Desc -> rbSortingDesc.isChecked = true
                 }
 
                 when (it.sortingType) {
-                    SortingType.Alphabetically -> {
-                        tvAlphabetically.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_round_check_circle_24, 0, 0, 0)
-                        tvCreationDate.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_round_circle_24, 0, 0, 0)
-                    }
-                    SortingType.CreationDate -> {
-                        tvCreationDate.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_round_check_circle_24, 0, 0, 0)
-                        tvAlphabetically.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_round_circle_24, 0, 0, 0)
-                    }
-
+                    SortingType.Alphabetically -> rbAlphabetically.isChecked = true
+                    SortingType.CreationDate -> rbCreationDate.isChecked = true
                 }
             }
             .launchIn(lifecycleScope)
 
-
-        val showAnim = AlphaAnimation(0F, 1F).apply {
-            duration = 250
-        }
-
-        rgSortingMethod.setOnCheckedChangeListener { _, checkedId ->
-            when (checkedId) {
-                rbSortingDesc.id -> rbSortingDesc.startAnimation(showAnim)
-                rbSortingAsc.id -> rbSortingAsc.startAnimation(showAnim)
-            }
-        }
-
         rbSortingAsc.setOnClickListener { viewModel.updateSortingMethod(SortingMethod.Asc) }
         rbSortingDesc.setOnClickListener { viewModel.updateSortingMethod(SortingMethod.Desc) }
-        tvAlphabetically.setOnClickListener { viewModel.updateSortingType(SortingType.Alphabetically) }
-        tvCreationDate.setOnClickListener { viewModel.updateSortingType(SortingType.CreationDate) }
+        rbAlphabetically.setOnClickListener { viewModel.updateSortingType(SortingType.Alphabetically) }
+        rbCreationDate.setOnClickListener { viewModel.updateSortingType(SortingType.CreationDate) }
 
         btnConfirm.setOnClickListener {
             dismiss()
