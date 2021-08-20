d5acaa230d50212ea9a658dfd571b284c4be63d6
Ali Albaali <ali@albaali.com>
2021-08-20 22:19:16 +0300

    Implement LibraryListSortingDialogFragment for libraries sorting

---
+++ app/src/main/java/com/noto/app/main/LibraryListSortingDialogFragment.kt
@@ -0,0 +1,95 @@
+package com.noto.app.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import com.noto.app.BaseDialogFragment
+import com.noto.app.R
+import com.noto.app.databinding.BaseDialogFragmentBinding
+import com.noto.app.databinding.LibraryListSortingDialogFragmentBinding
+import com.noto.app.domain.model.LibraryListSorting
+import com.noto.app.domain.model.SortingOrder
+import com.noto.app.util.colorResource
+import com.noto.app.util.colorStateResource
+import com.noto.app.util.stringResource
+import com.noto.app.util.withBinding
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class LibraryListSortingDialogFragment : BaseDialogFragment() {
+
+    private val viewModel by viewModel<MainViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = LibraryListSortingDialogFragmentBinding.inflate(inflater, container, false).withBinding {
+
+        BaseDialogFragmentBinding.bind(root).apply {
+            tvDialogTitle.text = resources.stringResource(R.string.library_sorting)
+        }
+
+        viewModel.state
+            .onEach { state ->
+                when (state.sortingOrder) {
+                    SortingOrder.Ascending -> rbSortingAsc.isChecked = true
+                    SortingOrder.Descending -> rbSortingDesc.isChecked = true
+                }
+                when (state.sorting) {
+                    LibraryListSorting.Alphabetically -> {
+                        rbAlphabetically.isChecked = true
+                        enableSortingOrder()
+                    }
+                    LibraryListSorting.CreationDate -> {
+                        rbCreationDate.isChecked = true
+                        enableSortingOrder()
+                    }
+                    LibraryListSorting.Manually -> {
+                        rbManually.isChecked = true
+                        disableSortingOrder()
+                    }
+                }
+            }
+            .launchIn(lifecycleScope)
+
+        rbSortingAsc.setOnClickListener { viewModel.updateSortingOrder(SortingOrder.Ascending) }
+        rbSortingDesc.setOnClickListener { viewModel.updateSortingOrder(SortingOrder.Descending) }
+        rbManually.setOnClickListener { viewModel.updateSorting(LibraryListSorting.Manually) }
+        rbCreationDate.setOnClickListener { viewModel.updateSorting(LibraryListSorting.CreationDate) }
+        rbAlphabetically.setOnClickListener { viewModel.updateSorting(LibraryListSorting.Alphabetically) }
+
+        btnDone.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    private fun LibraryListSortingDialogFragmentBinding.disableSortingOrder() {
+        val disabledColor = resources.colorResource(R.color.colorOnPrimary)
+        val disableColorState = resources.colorStateResource(R.color.colorOnPrimary)
+        viewModel.updateSortingOrder(SortingOrder.Ascending)
+        rbSortingAsc.isChecked = true
+        rbSortingAsc.isEnabled = false
+        rbSortingDesc.isEnabled = false
+        rbSortingAsc.buttonTintList = disableColorState
+        rbSortingDesc.buttonTintList = disableColorState
+        rbSortingAsc.setTextColor(disabledColor)
+        rbSortingDesc.setTextColor(disabledColor)
+        tvSortingOrder.setTextColor(disabledColor)
+    }
+
+    private fun LibraryListSortingDialogFragmentBinding.enableSortingOrder() {
+        val enabledColor = resources.colorResource(R.color.colorPrimary)
+        val enabledColorState = resources.colorStateResource(R.color.colorPrimary)
+        rbSortingAsc.isEnabled = true
+        rbSortingDesc.isEnabled = true
+        rbSortingAsc.buttonTintList = enabledColorState
+        rbSortingDesc.buttonTintList = enabledColorState
+        rbSortingAsc.setTextColor(enabledColor)
+        rbSortingDesc.setTextColor(enabledColor)
+        tvSortingOrder.setTextColor(enabledColor)
+    }
+}
