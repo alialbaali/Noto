69ba540ca7573b9e1b46bd0d6883ec7162fe9391
Ali Albaali <ali@albaali.com>
2021-09-03 23:15:25 +0300

    Set cursor color based on library color in NoteFragment

--- app/src/main/java/com/noto/app/note/NoteFragment.kt
+++ app/src/main/java/com/noto/app/note/NoteFragment.kt
@@ -141,6 +141,10 @@ class NoteFragment : Fragment() {
             fab.outlineAmbientShadowColor = color
             fab.outlineSpotShadowColor = color
         }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            etNoteTitle.textCursorDrawable?.mutate()?.setTint(color)
+            etNoteBody.textCursorDrawable?.mutate()?.setTint(color)
+        }
     }
 
     @SuppressLint("SetTextI18n")
