18d59f631cb42811fc0bbac27d876a35ea8eea7c
Ali Albaali <ali@albaali.com>
2021-09-03 12:15:47 +0300

    Update Android gradle version to 7.0.2

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -22,7 +22,7 @@ object Versions {
     const val JavaTime = "1.1.5"
     const val Kotlin = "1.5.30"
     const val Navigation = "2.3.5"
-    const val Android = "7.0.1"
+    const val Android = "7.0.2"
     const val Koin = "3.1.2"
     const val Epoxy = "4.6.2"
 }
