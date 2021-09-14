909199e02338939d45cfcfcdb48cb44ec2ce3a07
Ali Albaali <ali@albaali.com>
2021-09-14 03:05:53 +0300

    Bump version to 1.4.1

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -28,8 +28,8 @@ object Versions {
 }
 
 object App {
-    const val VersionName = "1.4.0"
-    const val VersionCode = 20
+    const val VersionName = "1.4.1"
+    const val VersionCode = 21
     const val ID = "com.noto"
     const val MinSDK = 21
     const val CompileSDK = 30
