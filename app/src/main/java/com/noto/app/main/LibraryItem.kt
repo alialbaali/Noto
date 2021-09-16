dc28840c9d35271a09503c43a534308950bc4884
Ali Albaali <ali@albaali.com>
2021-09-16 17:40:27 +0300

    Set library title bottom margin to 0 when notes count is invisible

--- app/src/main/java/com/noto/app/main/LibraryItem.kt
+++ app/src/main/java/com/noto/app/main/LibraryItem.kt
@@ -3,7 +3,10 @@ package com.noto.app.main
 import android.annotation.SuppressLint
 import android.os.Build
 import android.view.View
+import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updateMarginsRelative
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyHolder
 import com.airbnb.epoxy.EpoxyModelClass
@@ -11,10 +14,7 @@ import com.airbnb.epoxy.EpoxyModelWithHolder
 import com.noto.app.R
 import com.noto.app.databinding.LibraryItemBinding
 import com.noto.app.domain.model.Library
-import com.noto.app.util.colorResource
-import com.noto.app.util.stringResource
-import com.noto.app.util.toCountText
-import com.noto.app.util.toResource
+import com.noto.app.util.*
 
 @SuppressLint("NonConstantResourceId")
 @EpoxyModelClass(layout = R.layout.library_item)
@@ -41,33 +41,37 @@ abstract class LibraryItem : EpoxyModelWithHolder<LibraryItem.Holder>() {
     @EpoxyAttribute
     var notesCount: Int = 0
 
-    @SuppressLint("ClickableViewAccessibility")
-    override fun bind(holder: Holder) {
-        val resources = holder.binding.root.resources
-        val color = resources.colorResource(library.color.toResource())
-        holder.binding.tvLibraryTitle.text = library.title
-        holder.binding.tvLibraryNotesCount.text = notesCount.toCountText(resources.stringResource(R.string.note), resources.stringResource(R.string.notes))
-        holder.binding.ibDrag.isVisible = isManualSorting
-        holder.binding.vColor.background.setTint(color)
-        holder.binding.tvLibraryTitle.setTextColor(color)
-        holder.binding.tvLibraryNotesCount.setTextColor(color)
-        holder.binding.ibDrag.drawable?.mutate()?.setTint(color)
-        holder.binding.ibDrag.setOnTouchListener(onDragHandleTouchListener)
-        holder.binding.root.setOnClickListener(onClickListener)
-        holder.binding.root.setOnLongClickListener(onLongClickListener)
-        holder.binding.tvLibraryNotesCount.isVisible = isShowNotesCount
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            holder.binding.root.outlineAmbientShadowColor = color
-            holder.binding.root.outlineSpotShadowColor = color
-        }
-    }
+    override fun bind(holder: Holder) = holder.bind()
 
-    class Holder : EpoxyHolder() {
-        lateinit var binding: LibraryItemBinding
-            private set
+    inner class Holder : EpoxyHolder() {
+        private lateinit var binding: LibraryItemBinding
 
         override fun bindView(itemView: View) {
             binding = LibraryItemBinding.bind(itemView)
+        }
+
+        @SuppressLint("ClickableViewAccessibility")
+        fun bind() = with(binding) {
+            val resources = binding.root.resources
+            val color = resources.colorResource(library.color.toResource())
+            tvLibraryTitle.text = library.title
+            tvLibraryNotesCount.text = notesCount.toCountText(resources.stringResource(R.string.note), resources.stringResource(R.string.notes))
+            ibDrag.isVisible = isManualSorting
+            vColor.background.setTint(color)
+            tvLibraryTitle.setTextColor(color)
+            tvLibraryNotesCount.setTextColor(color)
+            ibDrag.drawable?.mutate()?.setTint(color)
+            ibDrag.setOnTouchListener(onDragHandleTouchListener)
+            root.setOnClickListener(onClickListener)
+            root.setOnLongClickListener(onLongClickListener)
+            tvLibraryNotesCount.isVisible = isShowNotesCount
+            tvLibraryTitle.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                updateMarginsRelative(bottom = if (isShowNotesCount) 4.dp else 0.dp)
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                root.outlineAmbientShadowColor = color
+                root.outlineSpotShadowColor = color
+            }
         }
     }
 }
