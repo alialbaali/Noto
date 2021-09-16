ae302538ebb20af770e619faab6cfb822eb7f4a0
Ali Albaali <ali@albaali.com>
2021-09-16 19:31:40 +0300

    Refactor NoteListSortingItem

--- app/src/main/java/com/noto/app/library/NoteListSortingItem.kt
+++ app/src/main/java/com/noto/app/library/NoteListSortingItem.kt
@@ -33,33 +33,33 @@ abstract class NoteListSortingItem : EpoxyModelWithHolder<NoteListSortingItem.Ho
     @EpoxyAttribute
     lateinit var notoColor: NotoColor
 
-    override fun bind(holder: Holder) {
-        val resources = holder.binding.root.resources
+    override fun bind(holder: Holder) = with(holder.binding) {
+        val resources = root.resources
         val sortingText = resources.stringResource(R.string.sorting).lowercase()
         val color = resources.colorResource(notoColor.toResource())
-        holder.binding.tvSorting.text = when (sorting) {
+        tvSorting.text = when (sorting) {
             NoteListSorting.Manual -> "${resources.stringResource(R.string.manual)} $sortingText"
             NoteListSorting.CreationDate -> "${resources.stringResource(R.string.creation_date)} $sortingText"
             NoteListSorting.Alphabetical -> "${resources.stringResource(R.string.alphabetical)} $sortingText"
         }
-        holder.binding.tvSorting.setOnClickListener(onClickListener)
-        holder.binding.root.rootView.setFullSpan()
+        tvSorting.setOnClickListener(onClickListener)
+        root.rootView.setFullSpan()
         val arrowDrawable = when (sortingOrder) {
             SortingOrder.Ascending -> R.drawable.ic_round_arrow_up_24
             SortingOrder.Descending -> R.drawable.ic_round_arrow_down_24
         }
         if (sorting == NoteListSorting.Manual)
-            holder.binding.tvSorting.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+            tvSorting.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
         else
-            holder.binding.tvSorting.setCompoundDrawablesWithIntrinsicBounds(arrowDrawable, 0, 0, 0)
-        holder.binding.tvLibraryNotesCount.text = notesCount.toCountText(
+            tvSorting.setCompoundDrawablesWithIntrinsicBounds(arrowDrawable, 0, 0, 0)
+        tvLibraryNotesCount.text = notesCount.toCountText(
             resources.stringResource(R.string.note),
             resources.stringResource(R.string.notes)
         )
-        holder.binding.tvLibraryNotesCount.setTextColor(color)
-        holder.binding.tvSorting.setTextColor(color)
-        holder.binding.tvSorting.background?.mutate()?.setTint(ColorUtils.setAlphaComponent(color, 25))
-        holder.binding.tvSorting.compoundDrawables[0]?.mutate()?.setTint(color)
+        tvSorting.background?.mutate()?.setTint(ColorUtils.setAlphaComponent(color, 25))
+        tvSorting.compoundDrawables[0]?.mutate()?.setTint(color)
+        tvLibraryNotesCount.setTextColor(color)
+        tvSorting.setTextColor(color)
     }
 
     class Holder : EpoxyHolder() {
