eed6fb0881e4d5f5f1fc7d7afa356851966d7c64
Ali Albaali <ali@albaali.com>
2021-09-19 15:49:04 +0300

    Add new_label_item

---
+++ app/src/main/java/com/noto/app/label/NewLabelItem.kt
@@ -0,0 +1,49 @@
+package com.noto.app.label
+
+import android.annotation.SuppressLint
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.RippleDrawable
+import android.view.View
+import com.airbnb.epoxy.EpoxyAttribute
+import com.airbnb.epoxy.EpoxyHolder
+import com.airbnb.epoxy.EpoxyModelClass
+import com.airbnb.epoxy.EpoxyModelWithHolder
+import com.noto.app.R
+import com.noto.app.databinding.NewLabelItemBinding
+import com.noto.app.domain.model.NotoColor
+import com.noto.app.util.*
+
+@SuppressLint("NonConstantResourceId")
+@EpoxyModelClass(layout = R.layout.new_label_item)
+abstract class NewLabelItem : EpoxyModelWithHolder<NewLabelItem.Holder>() {
+
+    @EpoxyAttribute
+    lateinit var color: NotoColor
+
+    @EpoxyAttribute(EpoxyAttribute.Option.DoNotHash)
+    lateinit var onClickListener: View.OnClickListener
+
+    override fun bind(holder: Holder) = with(holder.binding) {
+        ibNewLabel.setOnClickListener(onClickListener)
+        val resourceColor = root.resources.colorResource(color.toResource())
+        ibNewLabel.imageTintList = root.resources.colorStateResource(color.toResource())
+        ibNewLabel.background = root.resources.drawableResource(R.drawable.label_item_shape)
+            ?.mutate()
+            ?.let { it as RippleDrawable }
+            ?.let { it.getDrawable(0) as GradientDrawable }
+            ?.apply {
+                setStroke(LabelDefaultStrokeWidth, resourceColor)
+                cornerRadius = LabelDefaultCornerRadius
+            }
+            ?.toRippleDrawable(root.resources)
+    }
+
+    class Holder : EpoxyHolder() {
+        lateinit var binding: NewLabelItemBinding
+            private set
+
+        override fun bindView(itemView: View) {
+            binding = NewLabelItemBinding.bind(itemView)
+        }
+    }
+}
