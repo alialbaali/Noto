9f05a0f654fe83e4b072bdcd1c3b5a938ec2f641
Ali Albaali <ali@albaali.com>
2021-09-29 00:30:17 +0300

    Refactor and redesign sorting dialog fragment

--- app/src/main/java/com/noto/app/library/NoteListSortingOrderDialogFragment.kt
+++ app/src/main/java/com/noto/app/library/NoteListSortingOrderDialogFragment.kt
@@ -9,28 +9,28 @@ import androidx.navigation.fragment.navArgs
 import com.noto.app.BaseDialogFragment
 import com.noto.app.R
 import com.noto.app.databinding.BaseDialogFragmentBinding
-import com.noto.app.databinding.NoteListSortingDialogFragmentBinding
-import com.noto.app.domain.model.NoteListSorting
+import com.noto.app.databinding.NoteListSortingOrderDialogFragmentBinding
+import com.noto.app.domain.model.SortingOrder
 import com.noto.app.util.*
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
-class NoteListSortingDialogFragment : BaseDialogFragment() {
+class NoteListSortingOrderDialogFragment : BaseDialogFragment() {
 
     private val viewModel by viewModel<LibraryViewModel> { parametersOf(args.libraryId) }
 
-    private val args by navArgs<NoteListSortingDialogFragmentArgs>()
+    private val args by navArgs<NoteListSortingOrderDialogFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = NoteListSortingDialogFragmentBinding.inflate(inflater, container, false).withBinding {
+    ): View = NoteListSortingOrderDialogFragmentBinding.inflate(inflater, container, false).withBinding {
 
         val baseDialog = BaseDialogFragmentBinding.bind(root).apply {
-            tvDialogTitle.text = resources.stringResource(R.string.sorting)
+            tvDialogTitle.text = resources.stringResource(R.string.sorting_order)
         }
 
         viewModel.library
@@ -39,30 +39,23 @@ class NoteListSortingDialogFragment : BaseDialogFragment() {
                 val colorStateList = resources.colorStateResource(library.color.toResource())
                 baseDialog.tvDialogTitle.setTextColor(color)
                 baseDialog.vHead.background?.mutate()?.setTint(color)
-                rbManual.buttonTintList = colorStateList
-                rbAlphabetical.buttonTintList = colorStateList
-                rbCreationDate.buttonTintList = colorStateList
-                when (library.sorting) {
-                    NoteListSorting.Alphabetical -> rbAlphabetical.isChecked = true
-                    NoteListSorting.CreationDate -> rbCreationDate.isChecked = true
-                    NoteListSorting.Manual -> rbManual.isChecked = true
+                rbSortingAsc.buttonTintList = colorStateList
+                rbSortingDesc.buttonTintList = colorStateList
+                when (library.sortingOrder) {
+                    SortingOrder.Ascending -> rbSortingAsc.isChecked = true
+                    SortingOrder.Descending -> rbSortingDesc.isChecked = true
                 }
             }
             .launchIn(lifecycleScope)
 
-        rbManual.setOnClickListener {
+        rbSortingAsc.setOnClickListener {
             dismiss()
-            viewModel.updateSorting(NoteListSorting.Manual)
+            viewModel.updateSortingOrder(SortingOrder.Ascending)
         }
 
-        rbCreationDate.setOnClickListener {
+        rbSortingDesc.setOnClickListener {
             dismiss()
-            viewModel.updateSorting(NoteListSorting.CreationDate)
-        }
-
-        rbAlphabetical.setOnClickListener {
-            dismiss()
-            viewModel.updateSorting(NoteListSorting.Alphabetical)
+            viewModel.updateSortingOrder(SortingOrder.Descending)
         }
     }
 }
