bb7db1f096b5f15d91e2e5f3fb0339e59d6af109
Ali Albaali <ali@albaali.com>
2021-10-03 05:19:12 +0300

    Fix note_label_item height by using margins

--- app/src/main/java/com/noto/app/library/NoteItem.kt
+++ app/src/main/java/com/noto/app/library/NoteItem.kt
@@ -12,7 +12,6 @@ import com.airbnb.epoxy.EpoxyModelClass
 import com.airbnb.epoxy.EpoxyModelWithHolder
 import com.google.android.flexbox.FlexDirection
 import com.google.android.flexbox.FlexWrap
-import com.google.android.flexbox.FlexboxItemDecoration
 import com.google.android.flexbox.FlexboxLayoutManager
 import com.noto.app.R
 import com.noto.app.databinding.NoteItemBinding
@@ -71,13 +70,6 @@ abstract class NoteItem : EpoxyModelWithHolder<NoteItem.Holder>() {
         ibDrag.setOnTouchListener(onDragHandleTouchListener)
         rv.isVisible = labels.isNotEmpty()
         rv.layoutManager = FlexboxLayoutManager(root.context, FlexDirection.ROW, FlexWrap.WRAP)
-        if (rv.itemDecorationCount == 0) {
-            val itemDecoration = FlexboxItemDecoration(root.context).apply {
-                setDrawable(root.resources.drawableResource(R.drawable.note_label_item_decoration))
-                setOrientation(FlexboxItemDecoration.HORIZONTAL)
-            }
-            rv.addItemDecoration(itemDecoration)
-        }
         rv.withModels {
             labels.forEach { label ->
                 noteLabelItem {
