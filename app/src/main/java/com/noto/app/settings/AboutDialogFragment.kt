aa6e43c22eea2ba6362e3f410058cd656689156d
Ali Albaali <ali@albaali.com>
2021-10-05 18:52:57 +0300

    Add About dialog

---
+++ app/src/main/java/com/noto/app/settings/AboutDialogFragment.kt
@@ -0,0 +1,34 @@
+package com.noto.app.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.noto.app.BaseDialogFragment
+import com.noto.app.R
+import com.noto.app.databinding.AboutDialogFragmentBinding
+import com.noto.app.databinding.BaseDialogFragmentBinding
+import com.noto.app.util.stringResource
+import com.noto.app.util.withBinding
+
+class AboutDialogFragment : BaseDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = AboutDialogFragmentBinding.inflate(inflater, container, false).withBinding {
+        setupBaseDialogFragment()
+        setupListeners()
+    }
+
+    fun AboutDialogFragmentBinding.setupBaseDialogFragment() = BaseDialogFragmentBinding.bind(root).apply {
+        tvDialogTitle.text = resources.stringResource(R.string.about)
+    }
+
+    fun AboutDialogFragmentBinding.setupListeners() {
+        btnOkay.setOnClickListener {
+            dismiss()
+        }
+    }
+}
