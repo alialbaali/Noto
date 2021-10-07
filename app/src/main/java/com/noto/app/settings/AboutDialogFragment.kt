f855cac5dc3ea535ee8d2fbb02eebc0dc4e0b446
Ali Albaali <ali@albaali.com>
2021-10-07 15:43:56 +0300

    Update About dialog content

--- app/src/main/java/com/noto/app/settings/AboutDialogFragment.kt
+++ app/src/main/java/com/noto/app/settings/AboutDialogFragment.kt
@@ -1,6 +1,7 @@
 package com.noto.app.settings
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import com.noto.app.BaseDialogFragment
 import com.noto.app.R
 import com.noto.app.databinding.AboutDialogFragmentBinding
 import com.noto.app.databinding.BaseDialogFragmentBinding
+import com.noto.app.util.removeLinksUnderline
 import com.noto.app.util.stringResource
 import com.noto.app.util.withBinding
 
@@ -19,11 +21,17 @@ class AboutDialogFragment : BaseDialogFragment() {
         savedInstanceState: Bundle?
     ): View = AboutDialogFragmentBinding.inflate(inflater, container, false).withBinding {
         setupBaseDialogFragment()
+        setupState()
         setupListeners()
     }
 
     fun AboutDialogFragmentBinding.setupBaseDialogFragment() = BaseDialogFragmentBinding.bind(root).apply {
         tvDialogTitle.text = resources.stringResource(R.string.about)
+    }
+
+    fun AboutDialogFragmentBinding.setupState() {
+        tvAbout.removeLinksUnderline()
+        tvAbout.movementMethod = LinkMovementMethod.getInstance()
     }
 
     fun AboutDialogFragmentBinding.setupListeners() {
--- app/src/main/java/com/noto/app/util/ViewUtils.kt
+++ app/src/main/java/com/noto/app/util/ViewUtils.kt
@@ -8,6 +8,9 @@ import android.graphics.Typeface
 import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.RippleDrawable
 import android.net.Uri
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.style.URLSpan
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
@@ -93,4 +96,22 @@ fun EditText.textAsFlow(emitNewTextOnly: Boolean = false): Flow<CharSequence?> {
         addTextChangedListener(listener)
         awaitClose { removeTextChangedListener(listener) }
     }.onStart { emit(text) }
+}
+
+fun TextView.removeLinksUnderline() {
+    val spannable = SpannableString(text)
+    for (urlSpan in spannable.getSpans(0, spannable.length, URLSpan::class.java)) {
+        spannable.setSpan(
+            object : URLSpan(urlSpan.url) {
+                override fun updateDrawState(ds: TextPaint) {
+                    super.updateDrawState(ds)
+                    ds.isUnderlineText = false
+                }
+            },
+            spannable.getSpanStart(urlSpan),
+            spannable.getSpanEnd(urlSpan),
+            0,
+        )
+    }
+    text = spannable
 }
