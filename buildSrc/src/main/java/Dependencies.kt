ce0e4a4313bf8e7ac00b185491c2a6d58be70ba8
Ali Albaali <ali@albaali.com>
2021-11-13 21:16:49 +0300

    Bump version to 1.6.3

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -29,8 +29,8 @@ object Versions {
 }
 
 object App {
-    const val VersionName = "1.6.2"
-    const val VersionCode = 28
+    const val VersionName = "1.6.3"
+    const val VersionCode = 29
     const val ID = "com.noto"
     const val MinSDK = 21
     const val CompileSDK = 31
