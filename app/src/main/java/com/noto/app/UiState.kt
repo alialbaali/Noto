f1477275fe54970f21f6cd063dfedfe1c56adb87
Ali Albaali <ali@albaali.com>
2021-12-17 07:47:20 +0300

    Add UiState sealed interface to represent loading state

---
+++ app/src/main/java/com/noto/app/UiState.kt
@@ -0,0 +1,14 @@
+package com.noto.app
+
+import com.noto.app.UiState.Loading
+import com.noto.app.UiState.Success
+
+sealed interface UiState<out T> {
+    object Loading : UiState<Nothing>
+    data class Success<T>(val value: T) : UiState<T>
+}
+
+inline fun <T, R> UiState<T>.map(transform: (value: T) -> R): UiState<R> = when (this) {
+    is Loading -> Loading
+    is Success -> Success(transform(value))
+}
