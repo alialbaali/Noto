67eebd22e53d42907a6fb473305cd30b4fe443ed
Ali Albaali <ali@albaali.com>
2022-01-02 23:23:56 +0300

    Add Release sealed interface and its UI

---
+++ app/src/main/java/com/noto/app/domain/model/Release.kt
@@ -0,0 +1,25 @@
+package com.noto.app.domain.model
+
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.Month
+
+sealed interface Release {
+    val version: String
+    val date: LocalDate
+    val changelog: String
+}
+
+data class Release_1_8_0(override val changelog: String) : Release {
+    override val version: String = "1.8.0"
+    override val date: LocalDate = LocalDate(2022, Month.JANUARY, 1)
+}
+
+class Release_1_9_0(override val changelog: String) : Release {
+    override val version: String = "1.9.0"
+    override val date: LocalDate = LocalDate(2022, Month.JANUARY, 1)
+}
+
+class Release_1_9_1(override val changelog: String) : Release {
+    override val version: String = "1.9.1"
+    override val date: LocalDate = LocalDate(2022, Month.JANUARY, 1)
+}
