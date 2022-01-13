a83b29683eedf1afb95addf4ea374250f83b9351
Ali Albaali <ali@albaali.com>
2022-01-13 05:31:23 +0300

    Redesign main fragment to be bottom sheet dialog

--- app/src/main/java/com/noto/app/main/LibraryItemTouchHelperCallback.kt
+++ app/src/main/java/com/noto/app/main/LibraryItemTouchHelperCallback.kt
@@ -4,21 +4,16 @@ import android.view.View
 import androidx.recyclerview.widget.ItemTouchHelper
 import com.airbnb.epoxy.EpoxyController
 import com.airbnb.epoxy.EpoxyModelTouchCallback
-import com.noto.app.domain.model.Layout
 
 class LibraryItemTouchHelperCallback(
     epoxyController: EpoxyController,
-    private val layout: Layout,
     private val callback: () -> Unit,
 ) : EpoxyModelTouchCallback<LibraryItem>(epoxyController, LibraryItem::class.java) {
 
     override fun isLongPressDragEnabled(): Boolean = false
 
     override fun getMovementFlagsForModel(model: LibraryItem?, adapterPosition: Int): Int {
-        val dragFlags = when (layout) {
-            Layout.Linear -> ItemTouchHelper.UP or ItemTouchHelper.DOWN
-            Layout.Grid -> ItemTouchHelper.UP or ItemTouchHelper.DOWN or ItemTouchHelper.START or ItemTouchHelper.END
-        }
+        val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
         return makeMovementFlags(dragFlags, 0)
     }
 
