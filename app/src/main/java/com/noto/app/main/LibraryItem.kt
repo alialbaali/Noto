cdea40095d1ace724ca580dc43f3f2c1650552c7
Ali Albaali <ali@albaali.com>
2022-01-13 05:32:35 +0300

    Redesign library item

--- app/src/main/java/com/noto/app/main/LibraryItem.kt
+++ app/src/main/java/com/noto/app/main/LibraryItem.kt
@@ -1,7 +1,7 @@
 package com.noto.app.main
 
 import android.annotation.SuppressLint
-import android.os.Build
+import android.graphics.drawable.RippleDrawable
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
@@ -14,10 +14,7 @@ import com.airbnb.epoxy.EpoxyModelWithHolder
 import com.noto.app.R
 import com.noto.app.databinding.LibraryItemBinding
 import com.noto.app.domain.model.Library
-import com.noto.app.util.colorResource
-import com.noto.app.util.dp
-import com.noto.app.util.pluralsResource
-import com.noto.app.util.toResource
+import com.noto.app.util.*
 
 @SuppressLint("NonConstantResourceId")
 @EpoxyModelClass(layout = R.layout.library_item)
@@ -41,6 +38,9 @@ abstract class LibraryItem : EpoxyModelWithHolder<LibraryItem.Holder>() {
     @EpoxyAttribute
     open var isLongClickable: Boolean = true
 
+    @EpoxyAttribute
+    open var isSelected: Boolean = false
+
     @EpoxyAttribute(EpoxyAttribute.Option.DoNotHash)
     lateinit var onClickListener: View.OnClickListener
 
@@ -54,26 +54,42 @@ abstract class LibraryItem : EpoxyModelWithHolder<LibraryItem.Holder>() {
     override fun bind(holder: Holder) = with(holder.binding) {
         root.context?.let { context ->
             val color = context.colorResource(library.color.toResource())
-            tvLibraryNotesCount.text = context.pluralsResource(R.plurals.notes_count, notesCount, notesCount).lowercase()
-            vColor.background.setTint(color)
-            tvLibraryTitle.setTextColor(color)
-            tvLibraryNotesCount.setTextColor(color)
+            val selectedColorStateList = color.withDefaultAlpha().toColorStateList()
+            val rippleDrawable = root.background as RippleDrawable
+            rippleDrawable.setColor(selectedColorStateList)
+            tvNotesCount.text = notesCount.toString()
+            tvTitle.setTextColor(color)
+            tvNotesCount.setTextColor(color)
             ibDrag.drawable?.mutate()?.setTint(color)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                root.outlineAmbientShadowColor = color
-                root.outlineSpotShadowColor = color
+            root.backgroundTintList = if (isSelected)
+                selectedColorStateList
+            else
+                context.attributeColoResource(R.attr.notoBackgroundColor).toColorStateList()
+            if (library.isInbox) {
+                tvTitle.text = context.stringResource(R.string.inbox)
+                ivIcon.setImageDrawable(context.drawableResource(R.drawable.ic_round_inbox_24))
+            } else {
+                tvTitle.text = library.title
+                ivIcon.setImageDrawable(context.drawableResource(R.drawable.ic_round_folder_24))
             }
+            ivIcon.imageTintList = color.toColorStateList()
         }
-        tvLibraryTitle.text = library.title
-        ibDrag.isVisible = isManualSorting
+        ibDrag.visibility = when {
+            isManualSorting && !library.isInbox -> View.VISIBLE
+            isManualSorting && library.isInbox -> View.INVISIBLE
+            else -> View.GONE
+        }
         ibDrag.setOnTouchListener(onDragHandleTouchListener)
         root.setOnClickListener(onClickListener)
         root.setOnLongClickListener(onLongClickListener)
         root.isClickable = isClickable
         root.isLongClickable = isLongClickable
-        tvLibraryNotesCount.isVisible = isShowNotesCount
-        tvLibraryTitle.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-            updateMarginsRelative(bottom = if (isShowNotesCount) 4.dp else 0.dp)
+        tvNotesCount.isVisible = isShowNotesCount
+        tvTitle.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+            updateMarginsRelative(end = if (!isShowNotesCount && !isManualSorting) 16.dp else 8.dp)
+        }
+        tvNotesCount.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+            updateMarginsRelative(end = if (isManualSorting) 8.dp else 0.dp)
         }
     }
 
