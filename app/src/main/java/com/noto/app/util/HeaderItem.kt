1fb4f0cb631e883012db096c7eb6d89c4f55ea22
Ali Albaali <ali@albaali.com>
2022-01-14 11:48:42 +0300

    Add an option in header item to change content visibility

--- app/src/main/java/com/noto/app/util/HeaderItem.kt
+++ app/src/main/java/com/noto/app/util/HeaderItem.kt
@@ -2,6 +2,7 @@ package com.noto.app.util
 
 import android.annotation.SuppressLint
 import android.view.View
+import androidx.core.view.isVisible
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyHolder
 import com.airbnb.epoxy.EpoxyModelClass
@@ -16,8 +17,17 @@ abstract class HeaderItem : EpoxyModelWithHolder<HeaderItem.Holder>() {
     @EpoxyAttribute
     lateinit var title: String
 
+    @EpoxyAttribute
+    open var isVisible = false
+
+    @EpoxyAttribute(EpoxyAttribute.Option.DoNotHash)
+    var onClickListener: View.OnClickListener? = null
+
     override fun bind(holder: Holder) = with(holder.binding) {
         tvTitle.text = title
+        ibVisibility.animate().rotation(if (isVisible) 180F else 0F)
+        ibVisibility.setOnClickListener(onClickListener)
+        ibVisibility.isVisible = onClickListener != null
     }
 
     override fun onViewAttachedToWindow(holder: Holder) {
