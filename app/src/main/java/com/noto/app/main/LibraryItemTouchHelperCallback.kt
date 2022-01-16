8172ff3ddfef5fac948f4775354448143f0ef809
Ali Albaali <ali@albaali.com>
2022-01-16 19:36:30 +0300

    Use state list animator to indicate library is being dragged

--- app/src/main/java/com/noto/app/main/LibraryItemTouchHelperCallback.kt
+++ app/src/main/java/com/noto/app/main/LibraryItemTouchHelperCallback.kt
@@ -1,9 +1,12 @@
 package com.noto.app.main
 
+import android.os.Build
 import android.view.View
 import androidx.recyclerview.widget.ItemTouchHelper
 import com.airbnb.epoxy.EpoxyController
 import com.airbnb.epoxy.EpoxyModelTouchCallback
+import com.noto.app.util.colorResource
+import com.noto.app.util.toResource
 
 class LibraryItemTouchHelperCallback(
     epoxyController: EpoxyController,
@@ -20,5 +23,25 @@ class LibraryItemTouchHelperCallback(
     override fun onModelMoved(fromPosition: Int, toPosition: Int, modelBeingMoved: LibraryItem?, itemView: View?) {
         super.onModelMoved(fromPosition, toPosition, modelBeingMoved, itemView)
         callback()
+    }
+
+    override fun onDragStarted(model: LibraryItem?, itemView: View?, adapterPosition: Int) {
+        super.onDragStarted(model, itemView, adapterPosition)
+        itemView?.isSelected = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val color = model?.library?.color?.toResource()
+            if (color != null) {
+                val resource = itemView?.context?.colorResource(color)
+                if (resource != null) {
+                    itemView.outlineAmbientShadowColor = resource
+                    itemView.outlineSpotShadowColor = resource
+                }
+            }
+        }
+    }
+
+    override fun onDragReleased(model: LibraryItem?, itemView: View?) {
+        super.onDragReleased(model, itemView)
+        itemView?.isSelected = false
     }
 }
