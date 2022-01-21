00de00c69336a3ae2d8c2b20ba5852c76d1b7ec6
Ali Albaali <ali@albaali.com>
2022-01-21 14:11:22 +0300

    Fix collapsing toolbar

--- app/src/main/java/com/noto/app/recentnotes/RecentNotesFragment.kt
+++ app/src/main/java/com/noto/app/recentnotes/RecentNotesFragment.kt
@@ -101,10 +101,7 @@ class RecentNotesFragment : Fragment() {
             .launchIn(lifecycleScope)
 
         viewModel.isCollapseToolbar
-            .onEach { isCollapseToolbar ->
-                if (!abl.isExpanded)
-                    abl.setExpanded(!isCollapseToolbar, false)
-            }
+            .onEach { isCollapseToolbar -> abl.setExpanded(!isCollapseToolbar && abl.isExpanded, false) }
             .launchIn(lifecycleScope)
 
         val menuItem = bab.menu.findItem(R.id.change_visibility)
