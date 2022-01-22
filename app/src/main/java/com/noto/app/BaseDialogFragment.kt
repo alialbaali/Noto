8dafd54dc68c1ed79d6e04e227fe1858344c8355
Ali Albaali <ali@albaali.com>
2022-01-22 23:58:50 +0300

    Set bottom sheet dialog peek height to 500 dp when it's collapsable

--- app/src/main/java/com/noto/app/BaseDialogFragment.kt
+++ app/src/main/java/com/noto/app/BaseDialogFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.noto.app.util.dp
 
 open class BaseDialogFragment(private val isCollapsable: Boolean = false) : BottomSheetDialogFragment() {
 
@@ -13,7 +14,9 @@ open class BaseDialogFragment(private val isCollapsable: Boolean = false) : Bott
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return super.onCreateDialog(savedInstanceState).apply {
             (this as BottomSheetDialog).apply {
-                if (!isCollapsable) {
+                if (isCollapsable) {
+                    behavior.peekHeight = 500.dp
+                } else {
                     behavior.state = BottomSheetBehavior.STATE_EXPANDED
                     behavior.skipCollapsed = true
                 }
