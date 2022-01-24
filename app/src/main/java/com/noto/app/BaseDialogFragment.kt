78a902f881b99f72de36e8a00e0b96a3256e6f2c
Ali Albaali <ali@albaali.com>
2022-01-24 14:07:20 +0300

    Fix navigation bar color in bottom sheet dialog fragment

--- app/src/main/java/com/noto/app/BaseDialogFragment.kt
+++ app/src/main/java/com/noto/app/BaseDialogFragment.kt
@@ -1,10 +1,14 @@
 package com.noto.app
 
 import android.app.Dialog
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.noto.app.util.colorResource
 
 open class BaseDialogFragment : BottomSheetDialogFragment() {
 
@@ -22,6 +26,20 @@ open class BaseDialogFragment : BottomSheetDialogFragment() {
     @Suppress("DEPRECATION")
     override fun onResume() {
         super.onResume()
-        dialog?.window?.decorView?.systemUiVisibility = 0
+        when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                dialog?.window?.decorView?.systemUiVisibility = 0
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    dialog?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                } else {
+                    context?.let { context ->
+                        dialog?.window?.statusBarColor = context.colorResource(android.R.color.black)
+                        dialog?.window?.navigationBarColor = context.colorResource(android.R.color.black)
+                    }
+                }
+            }
+        }
     }
 }
