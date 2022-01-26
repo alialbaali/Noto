0cd64eaa2f6616f9feea98ce0d017785d7780434
Ali Albaali <ali@albaali.com>
2022-01-26 23:08:32 +0300

    Add SettingsConfig to represent settings values

---
+++ app/src/main/java/com/noto/app/domain/model/SettingsConfig.kt
@@ -0,0 +1,21 @@
+package com.noto.app.domain.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SettingsConfig(
+    val theme: Theme,
+    val font: Font,
+    val language: Language,
+    val vaultPasscode: String?,
+    val vaultTimeout: VaultTimeout,
+    val scheduledVaultTimeout: VaultTimeout?,
+    val isVaultOpen: Boolean,
+    val isBioAuthEnabled: Boolean,
+    val lastVersion: String,
+    val sortingType: LibraryListSortingType,
+    val sortingOrder: SortingOrder,
+    val isCollapseToolbar: Boolean,
+    val isShowNotesCount: Boolean,
+    val mainLibraryId: Long,
+)
