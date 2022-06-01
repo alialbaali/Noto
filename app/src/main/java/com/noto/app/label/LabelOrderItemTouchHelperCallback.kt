ca6ef1f2e0561b15f0cfe3bee0aaf5e9fcd4ea7c
Ali Albaali <ali@albaali.com>
2022-06-01 19:00:18 +0300

    Highlight labels when dragging them

--- app/src/main/java/com/noto/app/label/LabelOrderItemTouchHelperCallback.kt
+++ app/src/main/java/com/noto/app/label/LabelOrderItemTouchHelperCallback.kt
@@ -1,9 +1,13 @@
 package com.noto.app.label
 
 import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.recyclerview.widget.ItemTouchHelper
 import com.airbnb.epoxy.EpoxyController
 import com.airbnb.epoxy.EpoxyModelTouchCallback
+import com.noto.app.R
+import com.noto.app.util.*
 
 class LabelOrderItemTouchHelperCallback(
     epoxyController: EpoxyController,
@@ -21,4 +25,39 @@ class LabelOrderItemTouchHelperCallback(
         super.onModelMoved(fromPosition, toPosition, modelBeingMoved, itemView)
         callback()
     }
+
+    override fun onDragStarted(model: LabelOrderItem?, itemView: View?, adapterPosition: Int) {
+        itemView?.context?.let { context ->
+            if (model != null) {
+                val selectedBackgroundColor = context.colorResource(model.color.toResource())
+                val selectedTextColor = context.colorAttributeResource(R.attr.notoBackgroundColor)
+                val backgroundColor = context.colorAttributeResource(R.attr.notoSurfaceColor)
+                val textColor = context.colorAttributeResource(R.attr.notoSecondaryColor)
+                val tvLabel = itemView.findViewById<TextView>(R.id.tv_label)
+                val ibDrag = itemView.findViewById<ImageButton>(R.id.ib_drag)
+                tvLabel.animateBackgroundColor(backgroundColor, selectedBackgroundColor)
+                tvLabel.animateTextColor(textColor, selectedTextColor)
+                ibDrag.imageTintList = selectedBackgroundColor.toColorStateList()
+            }
+        }
+        super.onDragStarted(model, itemView, adapterPosition)
+    }
+
+    override fun onDragReleased(model: LabelOrderItem?, itemView: View?) {
+        itemView?.context?.let { context ->
+            if (model != null) {
+                val selectedBackgroundColor = context.colorResource(model.color.toResource())
+                val selectedTextColor = context.colorAttributeResource(R.attr.notoBackgroundColor)
+                val backgroundColor = context.colorAttributeResource(R.attr.notoSurfaceColor)
+                val textColor = context.colorAttributeResource(R.attr.notoSecondaryColor)
+                val tvLabel = itemView.findViewById<TextView>(R.id.tv_label)
+                val ibDrag = itemView.findViewById<ImageButton>(R.id.ib_drag)
+                tvLabel.animateBackgroundColor(selectedBackgroundColor, backgroundColor)
+                tvLabel.animateTextColor(selectedTextColor, textColor)
+                ibDrag.imageTintList = textColor.toColorStateList()
+            }
+        }
+        super.onDragReleased(model, itemView)
+    }
+
 }
