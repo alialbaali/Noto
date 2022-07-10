be8ed791f6645c335034b763b020405ecb96e27c
Ali Albaali <ali@albaali.com>
2022-07-10 21:20:22 +0300

    Remove activity?.finish call from StartFragment

--- app/src/main/java/com/noto/app/StartFragment.kt
+++ app/src/main/java/com/noto/app/StartFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.addCallback
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -33,9 +32,6 @@ class StartFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? = context?.let { context ->
-        activity?.onBackPressedDispatcher?.addCallback {
-            activity?.finish()
-        }
         setupMixedTransitions()
         ComposeView(context).apply {
             isTransitionGroup = true
