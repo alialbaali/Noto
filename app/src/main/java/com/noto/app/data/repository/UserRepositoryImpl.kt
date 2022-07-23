77ff5d94f399c8b10d7551db79b2b134054b1dfb
Ali Albaali <ali@albaali.com>
2022-07-23 16:25:13 +0300

    Wrap suspend functions in UserRepository with IO dispatcher

--- app/src/main/java/com/noto/app/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/noto/app/data/repository/UserRepositoryImpl.kt
@@ -6,12 +6,16 @@ import com.noto.app.domain.repository.UserRepository
 import com.noto.app.domain.source.RemoteAuthDataSource
 import com.noto.app.domain.source.RemoteUserDataSource
 import com.noto.app.util.PasswordCryptoUtils
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.withContext
 
 class UserRepositoryImpl(
     private val remoteAuthDataSource: RemoteAuthDataSource,
     private val remoteUserDataSource: RemoteUserDataSource,
     private val settingsRepository: SettingsRepository,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : UserRepository {
 
     override val user: Flow<Result<User>> = combine(
@@ -20,64 +24,83 @@ class UserRepositoryImpl(
     ) { name, email -> User(name, email) }
         .map { Result.success(it) }
         .catch { emit(Result.failure(it)) }
+        .flowOn(dispatcher)
 
     override suspend fun registerUser(name: String, email: String, password: String): Result<Unit> = runCatching {
-        val passwordData = PasswordCryptoUtils.hashPassword(password)
-        val remoteAuthUser = remoteAuthDataSource.signUp(email, passwordData.encodedHashedPassword)
-        settingsRepository.updateId(remoteAuthUser.id)
-        settingsRepository.updateName(name)
-        settingsRepository.updateEmail(email)
-        settingsRepository.updatePasswordParameters(passwordData.encodedParameters)
+        withContext(dispatcher) {
+            val passwordData = PasswordCryptoUtils.hashPassword(password)
+            val remoteAuthUser = remoteAuthDataSource.signUp(email, passwordData.encodedHashedPassword)
+            settingsRepository.updateId(remoteAuthUser.id)
+            settingsRepository.updateName(name)
+            settingsRepository.updateEmail(email)
+            settingsRepository.updatePasswordParameters(passwordData.encodedParameters)
+        }
     }
 
     override suspend fun loginUser(email: String, password: String): Result<Unit> = runCatching {
-        val passwordData = remoteAuthDataSource.getPasswordParameters(email).run {
-            PasswordCryptoUtils.hashPassword(
-                password = password,
-                encodedParameters = passwordParameters,
-            )
+        withContext(dispatcher) {
+            val passwordData = remoteAuthDataSource.getPasswordParameters(email).run {
+                PasswordCryptoUtils.hashPassword(
+                    password = password,
+                    encodedParameters = passwordParameters,
+                )
+            }
+            val response = remoteAuthDataSource.login(email, passwordData.encodedHashedPassword)
+            settingsRepository.updateId(response.user.id)
+            settingsRepository.updateEmail(response.user.email)
+            settingsRepository.updateAccessToken(response.accessToken)
+            settingsRepository.updateRefreshToken(response.refreshToken)
+            val user = remoteUserDataSource.getUser()
+            settingsRepository.updateName(user.name)
         }
-        val response = remoteAuthDataSource.login(email, passwordData.encodedHashedPassword)
-        settingsRepository.updateId(response.user.id)
-        settingsRepository.updateEmail(response.user.email)
-        settingsRepository.updateAccessToken(response.accessToken)
-        settingsRepository.updateRefreshToken(response.refreshToken)
-        val user = remoteUserDataSource.getUser()
-        settingsRepository.updateName(user.name)
     }
 
     override suspend fun completeUserRegistration(accessToken: String, refreshToken: String): Result<Unit> = runCatching {
-        val id = settingsRepository.id.first()
-        val name = settingsRepository.name.first()
-        val passwordParameters = settingsRepository.passwordParameters.first()
-        settingsRepository.updateAccessToken(accessToken)
-        settingsRepository.updateRefreshToken(refreshToken)
-        remoteUserDataSource.createUser(id, name, passwordParameters)
-        settingsRepository.clearPasswordParameters()
+        withContext(dispatcher) {
+            val id = settingsRepository.id.first()
+            val name = settingsRepository.name.first()
+            val passwordParameters = settingsRepository.passwordParameters.first()
+            settingsRepository.updateAccessToken(accessToken)
+            settingsRepository.updateRefreshToken(refreshToken)
+            remoteUserDataSource.createUser(id, name, passwordParameters)
+            settingsRepository.clearPasswordParameters()
+        }
     }
 
     override suspend fun updateName(name: String): Result<Unit> = runCatching {
-        val id = settingsRepository.id.first()
-        remoteUserDataSource.updateName(id, name)
-        settingsRepository.updateName(name)
+        withContext(dispatcher) {
+            val id = settingsRepository.id.first()
+            remoteUserDataSource.updateName(id, name)
+            settingsRepository.updateName(name)
+        }
     }
 
     override suspend fun updateEmail(email: String): Result<Unit> = runCatching {
-        remoteAuthDataSource.updateEmail(email)
+        withContext(dispatcher) {
+            remoteAuthDataSource.updateEmail(email)
+        }
     }
 
     override suspend fun completeUpdatingEmail(
         email: String,
         accessToken: String,
         refreshToken: String,
-    ): Result<Unit> = runCatching { settingsRepository.updateEmail(email) }
+    ): Result<Unit> = runCatching {
+        withContext(dispatcher) {
+            settingsRepository.updateEmail(email)
+        }
+    }
 
     override suspend fun logOutUser(): Result<Unit> = runCatching {
-        remoteAuthDataSource.logOut()
+        withContext(dispatcher) {
+            remoteAuthDataSource.logOut()
+        }
     }
 
     override suspend fun deleteUser(): Result<Unit> = runCatching {
-        remoteAuthDataSource.delete()
+        withContext(dispatcher) {
+            remoteAuthDataSource.delete()
+        }
     }
 
 }
