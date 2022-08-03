b0fce60716c0609229ba309d6c199ccb5faf885a
Ali Albaali <ali@albaali.com>
2022-08-03 05:36:51 +0300

    Add language credits

--- app/src/main/java/com/noto/app/settings/AboutSettingsFragment.kt
+++ app/src/main/java/com/noto/app/settings/AboutSettingsFragment.kt
@@ -26,6 +26,13 @@ private const val LicenseWebsite = "https://www.apache.org/licenses/LICENSE-2.0"
 private const val CollectionOfGradientsWebsite = "https://www.figma.com/community/file/830405806109119447"
 private const val CreatorWebsite = "https://geoffreycrofte.com"
 private const val AppIconsLicenseWebsite = "https://creativecommons.org/licenses/by/4.0/"
+private const val TurkishTranslatorWebsite = "https://linkedin.com/in/nuraysabri"
+private const val TurkishProofreaderWebsite = "https://sakci.me"
+private const val SpanishTranslatorWebsite = "https://github.com/faus32"
+private const val FrenchTranslatorWebsite = "https://github.com/kernoeb"
+private const val FrenchTranslator2Website = "https://geoffreycrofte.com"
+private const val CrowdinWebsite = "https://crowdin.com/project/notoapp"
+private const val ArabicTranslatorWebsite = "https://www.alialbaali.com"
 
 class AboutSettingsFragment : Fragment() {
 
@@ -115,6 +122,121 @@ class AboutSettingsFragment : Fragment() {
                                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(AppIconsLicenseWebsite))
                                 startActivity(intent)
                             },
+                        )
+                    }
+
+                    SettingsSection {
+                        SettingsItem(
+                            title = stringResource(id = R.string.language),
+                            type = SettingsItemType.Text(stringResource(id = R.string.arabic)),
+                            onClick = {
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(CrowdinWebsite))
+                                startActivity(intent)
+                            }
+                        )
+
+                        SettingsItem(
+                            title = stringResource(id = R.string.translator),
+                            type = SettingsItemType.Text(stringResource(id = R.string.arabic_translator)),
+                            onClick = {
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(ArabicTranslatorWebsite))
+                                startActivity(intent)
+                            }
+                        )
+                    }
+
+                    SettingsSection {
+                        SettingsItem(
+                            title = stringResource(id = R.string.language),
+                            type = SettingsItemType.Text(stringResource(id = R.string.turkish)),
+                            onClick = {
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(CrowdinWebsite))
+                                startActivity(intent)
+                            }
+                        )
+
+                        SettingsItem(
+                            title = stringResource(id = R.string.translator),
+                            type = SettingsItemType.Text(stringResource(id = R.string.turkish_translator)),
+                            onClick = {
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(TurkishTranslatorWebsite))
+                                startActivity(intent)
+                            }
+                        )
+
+                        SettingsItem(
+                            title = stringResource(id = R.string.proofreader),
+                            type = SettingsItemType.Text(stringResource(id = R.string.turkish_proofreader)),
+                            onClick = {
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(TurkishProofreaderWebsite))
+                                startActivity(intent)
+                            }
+                        )
+                    }
+
+                    SettingsSection {
+                        SettingsItem(
+                            title = stringResource(id = R.string.language),
+                            type = SettingsItemType.Text(stringResource(id = R.string.german)),
+                            onClick = {
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(CrowdinWebsite))
+                                startActivity(intent)
+                            }
+                        )
+
+                        SettingsItem(
+                            title = stringResource(id = R.string.translator),
+                            type = SettingsItemType.Text(stringResource(id = R.string.german_translator)),
+                            onClick = {}
+                        )
+                    }
+
+                    SettingsSection {
+                        SettingsItem(
+                            title = stringResource(id = R.string.language),
+                            type = SettingsItemType.Text(stringResource(id = R.string.spanish)),
+                            onClick = {
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(CrowdinWebsite))
+                                startActivity(intent)
+                            }
+                        )
+
+                        SettingsItem(
+                            title = stringResource(id = R.string.translator),
+                            type = SettingsItemType.Text(stringResource(id = R.string.spanish_translator)),
+                            onClick = {
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(SpanishTranslatorWebsite))
+                                startActivity(intent)
+                            }
+                        )
+                    }
+
+                    SettingsSection {
+                        SettingsItem(
+                            title = stringResource(id = R.string.language),
+                            type = SettingsItemType.Text(stringResource(id = R.string.french)),
+                            onClick = {
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(CrowdinWebsite))
+                                startActivity(intent)
+                            }
+                        )
+
+                        SettingsItem(
+                            title = stringResource(id = R.string.translator),
+                            type = SettingsItemType.Text(stringResource(id = R.string.french_translator)),
+                            onClick = {
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(FrenchTranslatorWebsite))
+                                startActivity(intent)
+                            }
+                        )
+
+                        SettingsItem(
+                            title = stringResource(id = R.string.translator),
+                            type = SettingsItemType.Text(stringResource(id = R.string.french_translator2)),
+                            onClick = {
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(FrenchTranslator2Website))
+                                startActivity(intent)
+                            }
                         )
                     }
                 }
