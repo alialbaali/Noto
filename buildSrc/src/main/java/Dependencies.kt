b1e8fb0320899f5781fc9776b4ea030ec1883688
Ali Albaali <ali@albaali.com>
2022-10-14 15:29:22 -0400

    Bump version to 2.2.0-RC

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -19,8 +19,8 @@ object Versions {
 }
 
 object App {
-    const val VersionName = "2.2.0-Beta3"
-    const val VersionCode = 48
+    const val VersionName = "2.2.0-RC"
+    const val VersionCode = 49
     const val ID = "com.noto"
     const val MinSDK = 21
     const val CompileSDK = 32
