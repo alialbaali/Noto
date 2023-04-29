1d51742ecf2f95345d5d1f57c0d94f638defcd64
Ali Albaali <ali@albaali.com>
2023-04-28 23:59:12 -0400

    Replace BMC & Patreon with Support Noto option in settings

--- app/src/main/java/com/noto/app/settings/SettingsFragment.kt
+++ app/src/main/java/com/noto/app/settings/SettingsFragment.kt
@@ -9,17 +9,31 @@ import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.toFontFamily
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import com.noto.app.NotoTheme
@@ -33,7 +47,8 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 private const val PlayStoreUrl = "https://play.google.com/store/apps/details?id=com.noto"
 private const val GithubIssueUrl = "https://github.com/alialbaali/Noto/issues/new"
-private const val BuyMeACoffeeUrl = "https://www.buymeacoffee.com/alialbaali"
+const val SupportNotoUrl = "https://github.com/alialbaali/Noto#support"
+val SupportNotoColor = Color(0xFFE57373)
 
 class SettingsFragment : Fragment() {
 
@@ -68,7 +83,7 @@ class SettingsFragment : Fragment() {
                     ShareAndRateSection()
                     AboutSection()
                     Spacer(modifier = Modifier.weight(1F))
-                    BuyMeACoffeeImage()
+                    SupportNotoItem()
                 }
             }
         }
@@ -196,19 +211,40 @@ class SettingsFragment : Fragment() {
     }
 
     @Composable
-    private fun BuyMeACoffeeImage(modifier: Modifier = Modifier) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_bmc_full_logo),
-            contentDescription = stringResource(id = R.string.buy_me_a_coffee),
-            modifier = modifier
+    private fun SupportNotoItem(modifier: Modifier = Modifier) {
+        val interactionSource = remember { MutableInteractionSource() }
+        val indication = rememberRipple(color = SupportNotoColor)
+        val fontFamily = remember { Font(R.font.pacifico).toFontFamily() }
+        val colorFilter = remember { ColorFilter.tint(SupportNotoColor) }
+
+        Row(
+            modifier
                 .clip(MaterialTheme.shapes.small)
-                .clickable {
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(BuyMeACoffeeUrl))
+                .clickable(interactionSource, indication) {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(SupportNotoUrl))
                     startActivity(intent)
                 }
                 .padding(NotoTheme.dimensions.medium)
-                .height(30.dp)
+                .wrapContentHeight()
                 .fillMaxWidth(),
-        )
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_round_favorite_24),
+                contentDescription = stringResource(id = R.string.support_noto),
+                colorFilter = colorFilter,
+                modifier = Modifier.size(32.dp),
+            )
+
+            Spacer(modifier = Modifier.width(NotoTheme.dimensions.small))
+
+            Text(
+                text = stringResource(id = R.string.support_noto),
+                style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Thin),
+                color = SupportNotoColor,
+                fontFamily = fontFamily,
+            )
+        }
     }
 }
