5cc5191c7f6d342435698675a5d16f939752726b
Ali Albaali <ali@albaali.com>
2023-06-02 17:36:06 -0400

    Refactor BottomSheetDialog composable

--- app/src/main/java/com/noto/app/components/BottomSheetDialog.kt
+++ app/src/main/java/com/noto/app/components/BottomSheetDialog.kt
@@ -3,24 +3,32 @@ package com.noto.app.components
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.noto.app.NotoTheme
 import com.noto.app.dialog
 import com.noto.app.settings.SettingsViewModel
@@ -29,7 +37,6 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 private val TipHeight = 5.dp
 private val TipWidth = 30.dp
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun BaseDialogFragment.BottomSheetDialog(
     title: String,
@@ -42,8 +49,6 @@ fun BaseDialogFragment.BottomSheetDialog(
     val nestedScrollConnection = rememberNestedScrollInteropConnection()
     val viewModel by viewModel<SettingsViewModel>()
     val theme by viewModel.theme.collectAsState()
-    var headerHeight by remember { mutableStateOf(0.dp) }
-    val density = LocalDensity.current
     val isScrolling by remember { derivedStateOf { scrollState.value > 0 } }
     val elevation by animateDpAsState(
         targetValue = if (isScrolling) NotoTheme.dimensions.extraSmall else 0.dp,
@@ -53,38 +58,24 @@ fun BaseDialogFragment.BottomSheetDialog(
         Surface(
             shape = MaterialTheme.shapes.dialog,
             color = MaterialTheme.colorScheme.background,
+            modifier = Modifier
         ) {
-            Column(
-                modifier = modifier
-                    .nestedScroll(nestedScrollConnection)
-                    .verticalScroll(scrollState)
-                    .padding(top = headerHeight)
-                    .padding(NotoTheme.dimensions.medium),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                content()
-            }
+            Column {
+                Header(
+                    title = title,
+                    painter = painter,
+                    headerColor = headerColor,
+                    elevation = elevation,
+                )
 
-            Surface(
-                color = MaterialTheme.colorScheme.background,
-                shadowElevation = elevation,
-            ) {
-                Box(
-                    Modifier
-                        .onGloballyPositioned { coordinates ->
-                            with(density) {
-                                headerHeight = coordinates.size.height.toDp()
-                            }
-                        }
-                        .fillMaxWidth()
+                Column(
+                    modifier = modifier
+                        .nestedScroll(nestedScrollConnection)
+                        .verticalScroll(scrollState)
                         .padding(NotoTheme.dimensions.medium),
-                    contentAlignment = Alignment.Center
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    if (painter != null) Icon(painter = painter, contentDescription = title, modifier = Modifier.align(Alignment.TopStart))
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Tip(headerColor ?: MaterialTheme.colorScheme.primary)
-                        Title(title, headerColor ?: MaterialTheme.colorScheme.primary)
-                    }
+                    content()
                 }
             }
         }
@@ -92,12 +83,43 @@ fun BaseDialogFragment.BottomSheetDialog(
 }
 
 @Composable
-private fun ColumnScope.Tip(color: Color, modifier: Modifier = Modifier) {
+private fun Header(
+    title: String,
+    painter: Painter?,
+    headerColor: Color?,
+    elevation: Dp,
+    modifier: Modifier = Modifier,
+) {
+    Surface(
+        modifier = Modifier.zIndex(1F),
+        color = MaterialTheme.colorScheme.background,
+        shadowElevation = elevation,
+    ) {
+        Box(
+            modifier
+                .fillMaxWidth()
+                .padding(NotoTheme.dimensions.medium),
+            contentAlignment = Alignment.Center
+        ) {
+            if (painter != null) Icon(
+                painter = painter,
+                contentDescription = title,
+                modifier = Modifier.align(Alignment.TopStart)
+            )
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Tip(headerColor ?: MaterialTheme.colorScheme.primary)
+                Title(title, headerColor ?: MaterialTheme.colorScheme.primary)
+            }
+        }
+    }
+}
+
+@Composable
+private fun Tip(color: Color, modifier: Modifier = Modifier) {
     Box(
         modifier
             .padding(bottom = NotoTheme.dimensions.extraSmall)
             .size(TipWidth, TipHeight)
-            .align(Alignment.CenterHorizontally)
             .background(color, MaterialTheme.shapes.extraLarge)
     )
 }
