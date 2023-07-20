87fb46405082d72522ec6547de062e10cee29e22
Ali Albaali <ali@albaali.com>
2023-07-20 19:05:14 -0400

    Implement BottomSheetDialogItem for columns

--- app/src/main/java/com/noto/app/components/BottomSheetDialogItem.kt
+++ app/src/main/java/com/noto/app/components/BottomSheetDialogItem.kt
@@ -1,18 +1,21 @@
 package com.noto.app.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import com.noto.app.NotoTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RowScope.BottomSheetDialogItem(
     text: String,
@@ -20,13 +23,17 @@ fun RowScope.BottomSheetDialogItem(
     painter: Painter,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
+    rippleColor: Color = Color.Unspecified,
 ) {
+    val alpha by animateFloatAsState(targetValue = if (enabled) 1F else 0.5F)
     Surface(
         onClick = onClick,
         modifier = Modifier
+            .alpha(alpha)
             .weight(1F)
             .clip(MaterialTheme.shapes.small),
         enabled = enabled,
+        rippleColor = rippleColor,
     ) {
         Column(
             modifier = modifier
@@ -36,6 +43,76 @@ fun RowScope.BottomSheetDialogItem(
         ) {
             Icon(painter = painter, contentDescription = text)
             Text(text = text, style = MaterialTheme.typography.labelLarge)
+        }
+    }
+}
+
+@Composable
+fun ColumnScope.BottomSheetDialogItem(
+    text: String,
+    onClick: () -> Unit,
+    painter: Painter,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    rippleColor: Color = Color.Unspecified,
+) {
+    val alpha by animateFloatAsState(targetValue = if (enabled) 1F else 0.5F)
+    Surface(
+        onClick = onClick,
+        modifier = Modifier
+            .alpha(alpha)
+            .fillMaxWidth()
+            .clip(MaterialTheme.shapes.small),
+        enabled = enabled,
+        rippleColor = rippleColor,
+    ) {
+        Row(
+            modifier = modifier
+                .padding(NotoTheme.dimensions.medium),
+            horizontalArrangement = Arrangement.spacedBy(NotoTheme.dimensions.medium),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(painter = painter, contentDescription = text)
+            Text(text = text, style = MaterialTheme.typography.bodyLarge)
+        }
+    }
+}
+
+@Composable
+fun ColumnScope.BottomSheetDialogItem(
+    text: String,
+    onClick: () -> Unit,
+    painter: Painter,
+    value: String,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    rippleColor: Color = Color.Unspecified,
+) {
+    val alpha by animateFloatAsState(targetValue = if (enabled) 1F else 0.5F)
+    Surface(
+        onClick = onClick,
+        modifier = Modifier
+            .alpha(alpha)
+            .fillMaxWidth()
+            .clip(MaterialTheme.shapes.small),
+        enabled = enabled,
+        rippleColor = rippleColor,
+    ) {
+        Row(
+            modifier = modifier
+                .padding(NotoTheme.dimensions.medium),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(NotoTheme.dimensions.medium),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(painter = painter, contentDescription = text)
+                Text(text = text, style = MaterialTheme.typography.bodyLarge)
+            }
+
+            Text(text = value, style = MaterialTheme.typography.bodyLarge, color = MaterialTheme.colorScheme.secondary)
         }
     }
 }
