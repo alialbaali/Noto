4aa566cad6308f937e1a0d65464ac5512123da63
Ali Albaali <ali@albaali.com>
2023-07-20 20:38:42 -0400

    Implement custom Button composable

---
+++ app/src/main/java/com/noto/app/components/Button.kt
@@ -0,0 +1,31 @@
+package com.noto.app.components
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.noto.app.NotoTheme
+
+@Composable
+fun Button(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    containerColor: Color = MaterialTheme.colorScheme.primary,
+    contentColor: Color = MaterialTheme.colorScheme.onPrimary,
+) {
+    Button(
+        onClick,
+        modifier = Modifier.fillMaxWidth(),
+        shape = MaterialTheme.shapes.small,
+        colors = ButtonDefaults.buttonColors(containerColor = containerColor, contentColor = contentColor),
+        contentPadding = PaddingValues(NotoTheme.dimensions.medium),
+    ) {
+        Text(text = text, style = MaterialTheme.typography.titleMedium)
+    }
+}
