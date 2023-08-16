f95fb044fca80c4e97671bb087ef1d18573c3ba4
Ali Albaali <ali@albaali.com>
2023-08-16 00:50:50 -0400

    Migrate ProgressIndicatorDialogFragment to Compose

--- app/src/main/java/com/noto/app/components/ProgressIndicatorDialogFragment.kt
+++ app/src/main/java/com/noto/app/components/ProgressIndicatorDialogFragment.kt
@@ -4,9 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.navigation.fragment.navArgs
-import com.noto.app.databinding.ProgressIndicatorDialogFragmentBinding
-import com.noto.app.util.withBinding
+import com.noto.app.theme.NotoTheme
+
+private val ProgressIndicatorSize = 50.dp
+private val ProgressIndicatorStrokeWidth = 5.dp
 
 class ProgressIndicatorDialogFragment : BaseDialogFragment() {
 
@@ -16,13 +28,22 @@ class ProgressIndicatorDialogFragment : BaseDialogFragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View = ProgressIndicatorDialogFragmentBinding.inflate(inflater, container, false).withBinding {
-        setupState()
+    ): View? = context?.let { context ->
         isCancelable = false
-    }
-
-    private fun ProgressIndicatorDialogFragmentBinding.setupState() {
-        tb.tvDialogTitle.text = args.title
-        indicator.contentDescription = args.title
+        ComposeView(context).apply {
+            setContent {
+                BottomSheetDialog(title = args.title) {
+                    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .padding(NotoTheme.dimensions.medium)
+                                .size(ProgressIndicatorSize),
+                            strokeWidth = ProgressIndicatorStrokeWidth,
+                            strokeCap = StrokeCap.Round,
+                        )
+                    }
+                }
+            }
+        }
     }
 }
