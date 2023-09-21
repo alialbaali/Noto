e973ff9c5d9f6102151150e8f0947b995bf2ce3e
Ali Albaali <ali@albaali.com>
2023-09-20 23:58:45 -0400

    Fix Quick Note tile server for API 34

--- app/src/main/java/com/noto/app/note/QuickNoteTileService.kt
+++ app/src/main/java/com/noto/app/note/QuickNoteTileService.kt
@@ -1,20 +1,30 @@
 package com.noto.app.note
 
+import android.app.PendingIntent
 import android.content.Intent
 import android.os.Build
 import android.service.quicksettings.TileService
 import androidx.annotation.RequiresApi
 import com.noto.app.components.TransparentActivity
 import com.noto.app.util.Constants
+import com.noto.app.util.PendingIntentFlags
+
+private const val PendingIntentRequestCode = 0
 
 @RequiresApi(Build.VERSION_CODES.N)
 class QuickNoteTileService : TileService() {
+    @Suppress("DEPRECATION")
     override fun onClick() {
         val intent = Intent(Constants.Intent.ActionQuickNote, null, this, TransparentActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK
         }
+        val pendingIntent = PendingIntent.getActivity(applicationContext, PendingIntentRequestCode, intent, PendingIntentFlags)
         unlockAndRun {
-            startActivityAndCollapse(intent)
+            if (Build.VERSION.SDK_INT >= 34) {
+                startActivityAndCollapse(pendingIntent)
+            } else {
+                startActivityAndCollapse(intent)
+            }
         }
     }
 }
